01d5f5938492d82dfe0ad4e23a766603055186be
Rob Grzywinski <rob.grzywinski@ureeka.biz>
2022-07-27 16:14:54 -0500

    More believable & consistency

--- packages/web/src/ui/HomeTopbar.tsx
+++ packages/web/src/ui/HomeTopbar.tsx
@@ -1,10 +1,10 @@
-import { Box, Flex, Heading, Text } from "@chakra-ui/react";
-import { AuthAvatar } from "authUser/component/AuthAvatar";
-import { BsBook } from "react-icons/bs";
+import { Box, Flex, Heading, Text } from '@chakra-ui/react';
+import { BsBook } from 'react-icons/bs';
+
+import { AuthAvatar } from 'authUser/component/AuthAvatar';
 
 // ********************************************************************************
 export const HomeTopbar: React.FC = () => {
-
   return (
     <Box width='full' paddingY={4} borderBottom='1px solid #ccc'>
       <Flex alignItems='center' justifyContent='space-between' marginX='auto' width='full' maxWidth={1000} >
@@ -13,13 +13,12 @@ export const HomeTopbar: React.FC = () => {
           <Heading marginLeft={2} fontSize={24}>Notebook</Heading>
         </Flex>
         <Flex alignItems='center'>
-          <Text marginRight={6} color='#555' fontSize={18} fontWeight={500} >Something</Text>
-          <Text marginRight={6} color='#555' fontSize={18} fontWeight={500} >Something</Text>
-          <Text marginRight={6} color='#555' fontSize={18} fontWeight={500} >Something</Text>
+          <Text marginRight={6} color='#555' fontSize={18} fontWeight={500} >Features</Text>
+          <Text marginRight={6} color='#555' fontSize={18} fontWeight={500} >About</Text>
+          <Text marginRight={6} color='#555' fontSize={18} fontWeight={500} >GitHub</Text>
           <AuthAvatar avatarSize='md' buttonSize='sm' />
         </Flex>
       </Flex>
     </Box>
-
   );
 };
