6899e6f64ae52e6ea62680a2f7e658494222e73a
Rob Grzywinski <rob.grzywinski@ureeka.biz>
2022-07-27 11:14:01 -0500

    Consistency

--- packages/service-common/src/util/throttle.ts
+++ packages/service-common/src/util/throttle.ts
@@ -1,16 +1,16 @@
 // ********************************************************************************
-// Utility function for throttling execution of the given function
-export function throttle<T>(fn: (args: T) => void, time = 1000){
+// throttles execution of the specified function
+export const throttle = <T>(fn: (args: T) => void, time = 1000) => {
   let isWaiting = true;
 
   setTimeout(() => isWaiting = false, time);
 
   return (args: T) => {
-    if(isWaiting) { return/*nothing to do*/; }
+    if(isWaiting) return/*nothing to do*/;
 
     isWaiting = true;
     fn(args);
 
     setTimeout(() => isWaiting = false, time);
   };
-}
+};
