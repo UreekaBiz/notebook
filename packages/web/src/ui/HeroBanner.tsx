bdcae5fe5212eab211f55d6d29f9e8d7e56b7c96
ErickJoestar <erickborquez@gmail.com>
2022-07-27 13:39:05 -0500

    Styles

--- packages/web/src/ui/HeroBanner.tsx
+++ packages/web/src/ui/HeroBanner.tsx
@@ -3,7 +3,7 @@ import { Box, Flex, Heading, Text } from '@chakra-ui/react';
 // ********************************************************************************
 export const HeroBanner: React.FC =  () => {
   return (
-    <Box width='full' height='100vh' paddingTop={150}>
+    <Box width='full' paddingTop={150} paddingBottom={200}>
       <Box marginX='auto' width='full' maxWidth={1000}>
         <Flex marginX='auto' textAlign='center' direction='column' alignItems='flex-start' maxWidth={800}>
           <Heading fontSize={72} marginBottom={2}>Notebooks. Reimagined.</Heading>
