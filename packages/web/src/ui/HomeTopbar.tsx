35b552bb58797f8e95e84e3ca34c35c2f91a5fcd
Rob Grzywinski <rob.grzywinski@ureeka.biz>
2022-07-27 16:15:30 -0500

    Consistency

--- packages/web/src/ui/HomeTopbar.tsx
+++ packages/web/src/ui/HomeTopbar.tsx
@@ -7,7 +7,7 @@ import { AuthAvatar } from 'authUser/component/AuthAvatar';
 export const HomeTopbar: React.FC = () => {
   return (
     <Box width='full' paddingY={4} borderBottom='1px solid #ccc'>
-      <Flex alignItems='center' justifyContent='space-between' marginX='auto' width='full' maxWidth={1000} >
+      <Flex alignItems='center' justifyContent='space-between' marginX='auto' width='full' maxWidth={1000}>
         <Flex alignItems='center'>
           <BsBook size={24} />
           <Heading marginLeft={2} fontSize={24}>Notebook</Heading>
