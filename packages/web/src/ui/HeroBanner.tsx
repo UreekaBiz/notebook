70a5091ebee243b72c2665a74a60b73ef9672a4b
ErickJoestar <erickborquez@gmail.com>
2022-07-27 12:44:27 -0500

    Fix styles

--- packages/web/src/ui/HeroBanner.tsx
+++ packages/web/src/ui/HeroBanner.tsx
@@ -4,9 +4,9 @@ import { Box, Flex, Heading, Text } from '@chakra-ui/react';
 export const HeroBanner: React.FC =  () => {
   return (
     <Box width='full' paddingY={60} >
-      <Flex direction='column' alignItems='center' marginX='auto' width='full' maxWidth={500}>
+      <Flex direction='column' alignItems='center' marginX='auto' width='full' maxWidth={500} textAlign='center'>
         <Heading>Notebooks. Reimagined.</Heading>
-        <Text fontSize={24}>something something</Text>
+        <Text fontSize={24}>something somethingsomething somethingsomething</Text>
       </Flex>
     </Box>
   );
