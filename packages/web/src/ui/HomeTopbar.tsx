fbb074598ffc1d754c00b6b96c8097c485c8c018
ErickJoestar <erickborquez@gmail.com>
2022-07-28 13:28:08 -0500

    Styles

--- packages/web/src/ui/HomeTopbar.tsx
+++ packages/web/src/ui/HomeTopbar.tsx
@@ -6,16 +6,23 @@ import { AuthAvatar } from 'authUser/component/AuthAvatar';
 // ********************************************************************************
 export const HomeTopbar: React.FC = () => {
   return (
-    <Box width='full' paddingY={4} borderBottom='1px solid #ccc'>
-      <Flex alignItems='center' justifyContent='space-between' marginX='auto' width='full' maxWidth={1000}>
+    <Box width='full' borderBottom='1px solid #ccc'>
+      <Flex
+        alignItems='center'
+        justifyContent='space-between'
+        marginX='auto'
+        width='full'
+        maxWidth={1000}
+        height={20}
+      >
         <Flex alignItems='center'>
           <BsBook size={24} />
           <Heading marginLeft={2} fontSize={24}>Notebook</Heading>
         </Flex>
         <Flex alignItems='center'>
-          <Text marginRight={6} color='#555' fontSize={18} fontWeight={500}>Features</Text>
-          <Text marginRight={6} color='#555' fontSize={18} fontWeight={500}>About</Text>
-          <Text marginRight={6} color='#555' fontSize={18} fontWeight={500}>GitHub</Text>
+          <Text marginRight={6} color='#555' fontSize={16} fontWeight={500}>Features</Text>
+          <Text marginRight={6} color='#555' fontSize={16} fontWeight={500}>About</Text>
+          <Text marginRight={6} color='#555' fontSize={16} fontWeight={500}>GitHub</Text>
           <AuthAvatar avatarSize='md' buttonSize='sm' />
         </Flex>
       </Flex>
