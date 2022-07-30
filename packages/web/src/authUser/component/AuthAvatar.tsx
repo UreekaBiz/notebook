0499ccfca378a1426e3e5e053fc1fe07576c2092
Rob Grzywinski <rob.grzywinski@ureeka.biz>
2022-07-30 13:55:38 -0500

    Cleanup & Consistency

--- packages/web/src/authUser/component/AuthAvatar.tsx
+++ packages/web/src/authUser/component/AuthAvatar.tsx
@@ -2,7 +2,6 @@ import { Avatar, Button, Menu, MenuButton, MenuDivider, MenuItem, MenuList } fro
 import Link from 'next/link';
 import { AiOutlineFileText } from 'react-icons/ai';
 import { BiHomeAlt, BiLogOut } from 'react-icons/bi';
-import { BsGrid } from 'react-icons/bs';
 import { FiSettings } from 'react-icons/fi';
 
 import { isLoggedOut } from '@ureeka-notebook/web-service';
@@ -11,24 +10,20 @@ import { useAuthedUser } from 'authUser/hook/useAuthedUser';
 import { coreRoutes } from 'shared/routes';
 import { useRouter } from 'next/router';
 
+// displays the User's avatar and menu (ala a 'hamburger')
+// NOTE: the AuthUserService doesn't need to be initialized to use this component.
+//       While the service is being initialized this component won't be shown.
 // ********************************************************************************
 interface Props {
-  /** show a log in button when the user is not auth'ed. Defaults to true*/
+  /** show a login button when the User is not auth'ed. Defaults to true */
   showLogIn?: boolean;
 
   // FIXME: get the options from chakra
-  /** the size of the avatar*/
+  /** the size of the avatar */
   avatarSize?: 'sm' | 'md' | 'lg' | 'xl';
   /** the size of the login button */
   buttonSize?: 'sm' | 'md' | 'lg' | 'xl';
 }
-/**
- * Component that displays the user's avatar. This avatar functions as a Menu since
- * it will display a list of options when the user clicks on it. If enabled, a
- * button to log in will be displayed if the user is not auth'ed.
- */
-// NOTE: This component don't require the AuthUser Service to be initialized to
-//       work, while the service is being initialized this component won't be shown.
 export const AuthAvatar: React.FC<Props> = ({ avatarSize, buttonSize, showLogIn = true }) => {
   const authedUser = useAuthedUser();
 
@@ -37,7 +32,6 @@ export const AuthAvatar: React.FC<Props> = ({ avatarSize, buttonSize, showLogIn 
   // == Handlers ==================================================================
   const handleHomeClick = () => router.push(coreRoutes.root);
   const handleNotebooksClick = () => router.push(coreRoutes.notebook);
-  const handleCollectionsClick = () => {};
 
   const handleSettingsClick = () => router.push(coreRoutes.settings);
   const handleSignOutClick = () => router.push(coreRoutes.logout);
@@ -45,18 +39,18 @@ export const AuthAvatar: React.FC<Props> = ({ avatarSize, buttonSize, showLogIn 
   // == UI ========================================================================
   if(authedUser === undefined/*AuthUserService not initialized*/) return null/*don't render anything*/;
 
-  if(isLoggedOut(authedUser)){
+  if(isLoggedOut(authedUser)) {
     if(!showLogIn) return null/*don't render anything*/;
     return (
       <Link href={coreRoutes.login}>
         <Button colorScheme='blue' size={buttonSize}>
-            Login
+          Login
         </Button>
       </Link>
     );
+  } /* else -- User is auth'ed */
 
-  } // else -- user is auth'ed
-
+  // ..............................................................................
   return (
     <Menu computePositionOnMount/**prevents sideways overflow in parent container */>
       <MenuButton
@@ -67,11 +61,10 @@ export const AuthAvatar: React.FC<Props> = ({ avatarSize, buttonSize, showLogIn 
         marginLeft={4}
         _hover={{ cursor: 'pointer' }}
       />
-      {/* FIXME: Make that it so that options can be a prop from the component?*/}
+      {/* FIXME: make that it so that options can be a prop from the component?*/}
       <MenuList>
         <MenuItem icon={<BiHomeAlt />} onClick={handleHomeClick}>Home</MenuItem>
         <MenuItem icon={<AiOutlineFileText />} onClick={handleNotebooksClick}>Notebooks</MenuItem>
-        <MenuItem icon={<BsGrid />} onClick={handleCollectionsClick}>Collections</MenuItem>
 
         <MenuDivider />
         <MenuItem icon={<FiSettings />} onClick={handleSettingsClick}>Settings</MenuItem>
