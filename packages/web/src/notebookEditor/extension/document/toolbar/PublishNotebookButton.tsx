a840a53e950f7a7b8f4c8fb744110c6b0f04e347
Rob Grzywinski <rob.grzywinski@ureeka.biz>
2022-07-30 12:35:32 -0500

    Use extracted Document name by default

--- packages/web/src/notebookEditor/extension/document/toolbar/PublishNotebookButton.tsx
+++ packages/web/src/notebookEditor/extension/document/toolbar/PublishNotebookButton.tsx
@@ -6,6 +6,7 @@ import { getLogger, NotebookService, Logger } from '@ureeka-notebook/web-service
 
 import { EditorToolComponentProps } from 'notebookEditor/toolbar/type';
 import { useNotebookEditor } from 'notebookEditor/hook/useNotebookEditor';
+import { extractDocumentName } from '@ureeka-notebook/service-common';
 
 const log = getLogger(Logger.NOTEBOOK);
 
@@ -14,7 +15,7 @@ interface Props extends EditorToolComponentProps {/*no additional*/}
 export const PublishNotebookButton: React.FC<Props> = () => {
   // == States ====================================================================
   // FIXME: move to using correct Async states, toasts, etc.
-  const { notebookId, editorService } = useNotebookEditor();
+  const { notebookId, editor, editorService } = useNotebookEditor();
   const [isLoading, setIsLoading] = useState(false/*by contract*/);
 
   // == Handlers ==================================================================
@@ -23,7 +24,7 @@ export const PublishNotebookButton: React.FC<Props> = () => {
     setIsLoading(true);
 
     const version = editorService.getVersionIndex();
-    const title = 'Default Notebook Title'/*FIXME*/;
+    const title = extractDocumentName(editor.state.doc)/*default to extracted Document name*/;
     const snippet = ''/*FIXME*/;
     const image = ''/*FIXME*/;
     try {
