d04a8566cb7dd7857d2cc776d3368d53ddb16789
Rob Grzywinski <rob.grzywinski@ureeka.biz>
2022-07-30 11:27:12 -0500

    Match `pm-devtool`

--- packages/service-common/src/notebookEditor/proseMirror/extension/title.ts
+++ packages/service-common/src/notebookEditor/proseMirror/extension/title.ts
@@ -6,10 +6,8 @@ import { NodeGroup, NodeIdentifier, NodeName } from '../node';
 import { NotebookSchemaType } from '../schema';
 
 // ********************************************************************************
-// ================================================================================
-// -- Attribute -------------------------------------------------------------------
-
-// NOTE: This values must have matching types the ones defined in the Extension.
+// == Attribute ===================================================================
+// NOTE: this value must have matching types -- the ones defined in the Extension
 const TitleAttributeSpec = {
   [AttributeType.Id]: noNodeOrMarkSpecAttributeDefaultValue<NodeIdentifier>(),
   [AttributeType.InitialMarksSet]: noNodeOrMarkSpecAttributeDefaultValue<boolean>(),
@@ -26,21 +24,21 @@ const TitleAttributeSpec = {
   [AttributeType.MarginBottom]: noNodeOrMarkSpecAttributeDefaultValue<string>(),
   [AttributeType.MarginLeft]: noNodeOrMarkSpecAttributeDefaultValue<string>(),
   [AttributeType.MarginRight]: noNodeOrMarkSpecAttributeDefaultValue<string>(),
-
 };
 export type TitleAttributes = AttributesTypeFromNodeSpecAttributes<typeof TitleAttributeSpec>;
 
-// ================================================================================
+// == Spec ========================================================================
 // -- Node Spec -------------------------------------------------------------------
 export const TitleNodeSpec: NodeSpec = {
   name: NodeName.TITLE,
+
   group: NodeGroup.BLOCK,
   content: `${NodeName.TEXT}*`,
 };
 
 // -- Node Type -------------------------------------------------------------------
 // NOTE: this is the only way since PM does not provide a way to specify the type
-//       of the attributes
+//       of the Attributes
 export type TitleNodeType = ProseMirrorNode<NotebookSchemaType> & { attrs: TitleAttributes; };
 export const isTitleNode = (node: ProseMirrorNode<NotebookSchemaType>): node is TitleNodeType => node.type.name === NodeName.TITLE;
 
@@ -48,5 +46,6 @@ export const isTitleNode = (node: ProseMirrorNode<NotebookSchemaType>): node is 
 // -- Render Spec -----------------------------------------------------------------
 export const TitleNodeRendererSpec: NodeRendererSpec<TitleAttributes> = {
   tag: 'div',
-  attributes: {/*use the default renderer on all attributes*/},
+
+  attributes: {/*use the default renderer on all Attributes*/},
 };
