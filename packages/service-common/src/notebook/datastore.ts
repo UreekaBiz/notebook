5e5f528f2a86dff4d73697ae91369d224b8a480f
Rob Grzywinski <rob.grzywinski@ureeka.biz>
2022-08-01 13:03:03 -0500

    Consistency

--- packages/service-common/src/notebook/datastore.ts
+++ packages/service-common/src/notebook/datastore.ts
@@ -33,12 +33,12 @@ export type Notebook_Create = Readonly<Modify<Notebook_Storage, {
   createTimestamp: FieldValue/*server written*/;
   updateTimestamp: FieldValue/*server written*/;
 }>>;
-export type Notebook_Update = Modify<Pick<Notebook_Storage, 'updateTimestamp' | 'lastUpdatedBy'>, {
-  readonly updateTimestamp: FieldValue/*server written*/;
-}> & Partial<Omit<Notebook, 'deleted' | 'createTimestamp'| 'createdBy' | 'updateTimestamp'>>;
-export type Notebook_Delete = Modify<Pick<Notebook_Storage, 'deleted' | 'updateTimestamp' | 'lastUpdatedBy'>, {
-  readonly updateTimestamp: FieldValue/*server written*/;
-}>;
+export type Notebook_Update = Readonly<Modify<Pick<Notebook_Storage, 'updateTimestamp' | 'lastUpdatedBy'>, {
+  updateTimestamp: FieldValue/*server written*/;
+}>> & Partial<Omit<Notebook, 'deleted' | 'createTimestamp'| 'createdBy' | 'updateTimestamp'>>;
+export type Notebook_Delete = Readonly<Modify<Pick<Notebook_Storage, 'deleted' | 'updateTimestamp' | 'lastUpdatedBy'>, {
+  updateTimestamp: FieldValue/*server written*/;
+}>>;
 
 // -- Published Notebook ----------------------------------------------------------
 export type PublishedNotebook_Create = Readonly<Modify<PublishedNotebook_Storage, {
