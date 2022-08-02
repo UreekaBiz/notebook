84aa7dfbf53b155e3ad4841e0af73a5049cc67e7
Rob Grzywinski <rob.grzywinski@ureeka.biz>
2022-08-02 06:12:11 -0500

    Consistency

--- packages/web/src/user/util.ts
+++ packages/web/src/user/util.ts
@@ -2,7 +2,7 @@ import { isBlank, stringHashCode, UserProfilePrivate, UserProfilePublic } from '
 
 // ********************************************************************************
 // == Display name ================================================================
-// Gets the display name for the given User Profile.
+// gets the display name for the given User Profile
 export const getPublicDisplayName = (profile: UserProfilePublic) => getDisplayName(profile.firstName, profile.lastName);
 export const getPrivateDisplayName = (profile: UserProfilePrivate) => getDisplayName(profile.firstName, profile.lastName);
 const getDisplayName = (firstName?: string, lastName?: string) => {
@@ -14,7 +14,7 @@ const getDisplayName = (firstName?: string, lastName?: string) => {
   return `${firstName} ${lastName}`;
 };
 
-// Gets the initials for the given User Profile.
+// gets the initials for the given User Profile
 export const getPublicInitials = (profile: UserProfilePublic) => getInitials(profile.firstName, profile.lastName);
 export const getPrivateInitials = (profile: UserProfilePrivate) => getInitials(profile.firstName, profile.lastName);
 const getInitials = (firstName?: string, lastName?: string ) => {
@@ -26,7 +26,7 @@ const getInitials = (firstName?: string, lastName?: string ) => {
   return `${firstName!.charAt(0)}${lastName!.charAt(0)}`;
 };
 
-// ================================================================================
+// == Background Color ============================================================
 /** A collection of colors used as a background colors */
 const DEFAULT_IMAGE_BACKGROUND_COLORS = [
   '#F9EBC8',
@@ -35,12 +35,11 @@ const DEFAULT_IMAGE_BACKGROUND_COLORS = [
   '#A0BCC2',
 ];
 
-// Gets a background color from the given userId. This implementation ensures that
-// the returned value is consistent for a given userId.
+/** Gets a background color from the given userId. This implementation ensures that
+ *  the returned value is consistent for a given userId */
 export const getBackgroundImageColor = (userId: string) => {
   const hash = stringHashCode(userId);
   const index = hash % DEFAULT_IMAGE_BACKGROUND_COLORS.length;
 
   return DEFAULT_IMAGE_BACKGROUND_COLORS[index];
-
 };
