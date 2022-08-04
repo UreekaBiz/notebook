c1fb73707b239d3ea4f215820f614c464ce32912
Alan Rodz <s061199@hotmail.com>
2022-08-03 19:05:05 -0500

    match pm devtool

--- packages/web/src/notebookEditor/extension/markHolder/util.ts
+++ packages/web/src/notebookEditor/extension/markHolder/util.ts
@@ -2,7 +2,7 @@ import { ChainedCommands, Editor } from '@tiptap/core';
 import { Mark, MarkType } from 'prosemirror-model';
 import { Selection, TextSelection } from 'prosemirror-state';
 
-import { createMarkHolderNode, getMarkName, isMarkHolderNode, AttributeType, JSONNode, JSONMark, MarkHolderNodeType, MarkName, SchemaV2 } from '@ureeka-notebook/service-common';
+import { createMarkHolderNode, getMarkName, isMarkHolderNode, AttributeType, MarkHolderNodeType, MarkName, JSONNode, JSONMark, SchemaV2 } from '@ureeka-notebook/service-common';
 
 // ********************************************************************************
 // creates a MarkHolder Node holding the Marks corresponding to the given MarkNames
@@ -32,9 +32,9 @@ export const toggleMarkInMarkHolder = (selection: Selection, chain: () => Chaine
   let newMarksArray: Mark[] = [];
   const storedMarks  = markHolder.attrs.storedMarks ?? '[]'/*empty marks array*/;
 
-  if(parseStoredMarks(storedMarks).some(mark => mark.type.name === appliedMarkType.name)) {
+  if(parseStoredMarks(storedMarks).some(mark => getMarkName(mark) === appliedMarkType.name)) {
     // already included, remove it
-    newMarksArray = [...parseStoredMarks(storedMarks).filter(mark => mark.type.name !== appliedMarkType.name)];
+    newMarksArray = [...parseStoredMarks(storedMarks).filter(mark => getMarkName(mark) !== appliedMarkType.name)];
   } else {
     // not included yet, add it
     newMarksArray = [...parseStoredMarks(storedMarks), appliedMarkType.create()];
