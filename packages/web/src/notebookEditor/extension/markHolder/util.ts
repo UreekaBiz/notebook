6cc534249a0ffadbfb571229b26f17a354e16bbc
Alan Rodz <s061199@hotmail.com>
2022-08-04 18:13:06 -0500

    match pm-devtool

--- packages/web/src/notebookEditor/extension/markHolder/util.ts
+++ packages/web/src/notebookEditor/extension/markHolder/util.ts
@@ -1,8 +1,8 @@
 import { ChainedCommands, Editor } from '@tiptap/core';
 import { Mark, MarkType } from 'prosemirror-model';
-import { Selection, TextSelection } from 'prosemirror-state';
+import { TextSelection } from 'prosemirror-state';
 
-import { createMarkHolderNode, getMarkName, isMarkHolderNode, AttributeType, MarkName, MarkHolderNodeType, JSONMark, JSONNode, SchemaV2 } from '@ureeka-notebook/service-common';
+import { createMarkHolderNode, getMarkName, isMarkHolderNode, AttributeType, JSONMark, JSONNode, MarkHolderNodeType, MarkName, NotebookSchemaType } from '@ureeka-notebook/service-common';
 
 // ********************************************************************************
 // creates a MarkHolder Node holding the Marks corresponding to the given MarkNames
@@ -28,20 +28,21 @@ export const getMarkHolder = (editor: Editor) => {
 
 /** Toggles a mark in the mark holder. This should be used when a mark is added to
  *  an empty node. */
-export const toggleMarkInMarkHolder = (selection: Selection, chain: () => ChainedCommands, markHolder: MarkHolderNodeType, appliedMarkType: MarkType) => {
+export const toggleMarkInMarkHolder = (editor: Editor, chain: () => ChainedCommands, markHolder: MarkHolderNodeType, appliedMarkType: MarkType) => {
   let newMarksArray: Mark[] = [];
   const storedMarks  = markHolder.attrs[AttributeType.StoredMarks];
   if(!storedMarks) return false/*nothing to do*/;
 
-  if(parseStoredMarks(storedMarks).some(mark => getMarkName(mark) === appliedMarkType.name)) {
+  if(parseStoredMarks(editor.state.schema, storedMarks).some(mark => getMarkName(mark) === appliedMarkType.name)) {
     // already included, remove it
-    newMarksArray = [...parseStoredMarks(storedMarks).filter(mark => getMarkName(mark) !== appliedMarkType.name)];
+    newMarksArray = [...parseStoredMarks(editor.state.schema, storedMarks).filter(mark => getMarkName(mark) !== appliedMarkType.name)];
   } else {
     // not included yet, add it
-    newMarksArray = [...parseStoredMarks(storedMarks), appliedMarkType.create()];
+    newMarksArray = [...parseStoredMarks(editor.state.schema, storedMarks), appliedMarkType.create()];
   }
 
   return chain().focus().command((props) => {
+    const { selection } = editor.state;
     const { dispatch, tr } = props;
     if(!selection.$anchor.parent.isBlock) return false/*command cannot be executed, Selection parent is not a Block Node*/;
 
@@ -66,13 +67,13 @@ export const inMarkHolder = (editor: Editor, markName: MarkName) => {
   const storedMarks = markHolder.attrs[AttributeType.StoredMarks];
   if(!storedMarks) return false/*no stored Marks, false by definition*/;
 
-  return parseStoredMarks(storedMarks).some(mark => getMarkName(mark) === markName);
+  return parseStoredMarks(editor.state.schema, storedMarks).some(mark => getMarkName(mark) === markName);
 };
 
 /** Parses the stringified array of Marks and returns it as a {@link Mark} array*/
-export const parseStoredMarks = (stringifiedStoredMarks: string) => {
+export const parseStoredMarks = (schema: NotebookSchemaType, stringifiedStoredMarks: string) => {
   const JSONMarks = JSON.parse(stringifiedStoredMarks) as JSONMark[]/*by contract*/;
-  const storedMarks = JSONMarks.map(markName => Mark.fromJSON(SchemaV2, markName));
+  const storedMarks = JSONMarks.map(markName => Mark.fromJSON(schema, markName));
 
   return storedMarks;
 };
