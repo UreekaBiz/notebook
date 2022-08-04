23ef378028c2c796171e26b18742333b5b78483d
Alan Rodz <s061199@hotmail.com>
2022-08-04 18:16:46 -0500

    match pm-devtool

--- packages/web/src/notebookEditor/extension/strikethrough/command.ts
+++ packages/web/src/notebookEditor/extension/strikethrough/command.ts
@@ -2,7 +2,7 @@ import { CommandProps } from '@tiptap/core';
 
 import { getStrikethroughMarkType, MarkName, CommandFunctionType } from '@ureeka-notebook/service-common';
 
-import { getMarkHolder, toggleMarkInMarkHolder } from 'notebookEditor/extension/markHolder/util';
+import { toggleMarkInMarkHolder, getMarkHolder } from 'notebookEditor/extension/markHolder/util';
 
 // ********************************************************************************
 // NOTE: ambient module to ensure command is TypeScript-registered for TipTap
@@ -22,7 +22,7 @@ export const unsetStrikethroughCommand = () => ({ commands }: CommandProps) => c
 export const toggleStrikethroughCommand = () => ({ editor, chain, commands }: CommandProps) => {
   // if MarkHolder is defined toggle the mark inside it
   const markHolder = getMarkHolder(editor);
-  if(markHolder) return toggleMarkInMarkHolder(editor.state.selection, chain, markHolder, getStrikethroughMarkType(editor.schema))/*nothing else to do*/;
+  if(markHolder) return toggleMarkInMarkHolder(editor, chain, markHolder, getStrikethroughMarkType(editor.schema))/*nothing else to do*/;
   /* else -- MarkHolder is not present */
 
   return commands.toggleMark(MarkName.STRIKETHROUGH);
