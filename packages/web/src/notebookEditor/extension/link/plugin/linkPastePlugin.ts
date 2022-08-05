0b8141887a31a170efae985b9ee78e005cee7017
Alan Rodz <s061199@hotmail.com>
2022-08-05 16:21:44 -0500

    match pm-devtool

--- packages/web/src/notebookEditor/extension/link/plugin/linkPastePlugin.ts
+++ packages/web/src/notebookEditor/extension/link/plugin/linkPastePlugin.ts
@@ -3,9 +3,9 @@ import { find } from 'linkifyjs';
 import { Slice } from 'prosemirror-model';
 import { Plugin, PluginKey } from 'prosemirror-state';
 import { EditorView } from 'prosemirror-view';
-import { string } from 'yup';
 
-import { NotebookSchemaType, MarkName } from '@ureeka-notebook/web-service';
+import { urlSchema, NotebookSchemaType, MarkName } from '@ureeka-notebook/web-service';
+
 import { NoPluginState } from 'notebookEditor/model/type';
 
 // ********************************************************************************
@@ -51,12 +51,3 @@ export const linkPastePlugin = (editor: Editor): Plugin => {
     },
   });
 };
-
-// -- URL -------------------------------------------------------------------------
-// a string()-based URL schema that allows for 'localhost'. Specifically, it only
-// adds 'localhost' to the existing Yup regex
-// NOTE: the following bug is not fixed: https://github.com/jquense/yup/issues/224
-// REF: https://github.com/jquense/yup/blob/master/src/string.js#L9
-const URL_REGEXP = /^((https?|ftp):)?\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(localhost|((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i;
-const urlSchema = string()
-    .matches(URL_REGEXP, { excludeEmptyString: true/*matches .url() behavior*/ });
