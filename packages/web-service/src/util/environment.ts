9035ed23f71077132c5ec97081f4b9bc604558af
Rob Grzywinski <rob.grzywinski@ureeka.biz>
2022-08-07 11:11:43 -0500

    Refactor & reorder for sanity

--- packages/web-service/src/util/environment.ts
+++ packages/web-service/src/util/environment.ts
@@ -9,8 +9,9 @@ type Environment =
   | 'NEXT_PUBLIC_VERSION'
 
   // from `.env.template`
+  | 'NEXT_PUBLIC_ACTIVITY_IDLE_TIMEOUT'
+  | 'NEXT_PUBLIC_APPLICATION_URL'
   | 'NEXT_PUBLIC_ENVIRONMENT'
-  | 'NEXT_PUBLIC_LOG_LEVEL'
   | 'NEXT_PUBLIC_FIREBASE_API_KEY'
   | 'NEXT_PUBLIC_FIREBASE_APP_ID'
   | 'NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN'
@@ -20,32 +21,31 @@ type Environment =
   | 'NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID'
   | 'NEXT_PUBLIC_FIREBASE_PROJECT_ID'
   | 'NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET'
-  | 'NEXT_PUBLIC_APPLICATION_URL'
+  | 'NEXT_PUBLIC_LOG_LEVEL'
   | 'NEXT_PUBLIC_NOTEBOOK_VERSION_BATCH_SIZE'
   | 'NEXT_PUBLIC_SESSION_UPDATE_INTERVAL'
-  | 'NEXT_PUBLIC_ACTIVITY_IDLE_TIMEOUT'
   ;
 
 // --------------------------------------------------------------------------------
-export const hasEnv = (key: string) => (process.env[key] !== undefined);
+// CHECK: should this allow for any key or is this constrained version best?
+export const hasEnv = (key: Environment) => (process.env[key] !== undefined);
 
-export const getEnv = (key: Environment, defaultValue?: string): string => {
+// ................................................................................
+const convertEnv = <T>(key: Environment, defaultValue: T | undefined/*not specified*/, convert: (value: string) => T): T => {
   if(!hasEnv(key)) {
     if(defaultValue !== undefined) return defaultValue;
     throw new ApplicationError('functions/not-found', `Unknown environment variable '${key}'.`);
   } /* else -- the key exists as expected */
-  const value = process.env[key]!;
-
-  return value;
+  return convert(process.env[key]!);
 };
-export const getEnvNumber = (key: Environment, defaultValue?: number): number => {
-  if(!hasEnv(key)) {
-    if(defaultValue !== undefined) return defaultValue;
-    throw new ApplicationError('functions/not-found', `Unknown environment variable '${key}'.`);
-  } /* else -- the key exists as expected */
-  const stringValue = process.env[key]!,
-        numberValue = Number(stringValue);
-  if(isNaN(numberValue)) throw new ApplicationError('functions/invalid-argument', `Expected number for environment variable '${key}' but got '${stringValue}'.`);
 
-  return numberValue;
-};
+export const getEnv = (key: Environment, defaultValue?: string): string =>
+  convertEnv(key, defaultValue, (value) => value);
+export const getEnvNumber = (key: Environment, defaultValue?: number): number =>
+  convertEnv(key, defaultValue, (value) => {
+    const numberValue = Number(value);
+    if(isNaN(numberValue)) throw new ApplicationError('functions/invalid-argument', `Expected number for environment variable '${key}' but got '${value}'.`);
+    return numberValue;
+  });
+export const getEnvBoolean = (key: Environment, defaultValue?: boolean): boolean =>
+  convertEnv(key, defaultValue, (value) => (value.toLowerCase() === 'true'));
