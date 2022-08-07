fcd5ffae11e5a5dcc31a8cce6e60b7e2b8c5de22
Rob Grzywinski <rob.grzywinski@ureeka.biz>
2022-08-07 10:27:24 -0500

    Expose environment

--- packages/web-service/src/util/index.ts
+++ packages/web-service/src/util/index.ts
@@ -1,5 +1,6 @@
 // ** Local ***********************************************************************
 // NOTE: './firebase' is exported from the root to ensure that it's processed first
+export * from './environment';
 export * from './error';
 export * from './version';
 
