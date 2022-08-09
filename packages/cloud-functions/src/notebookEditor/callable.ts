80bc189731448445c8ab25c2b82e61d84b67b313
ErickJoestar <erickborquez@gmail.com>
2022-08-09 11:26:43 -0500

    Consistency

--- packages/cloud-functions/src/notebookEditor/callable.ts
+++ packages/cloud-functions/src/notebookEditor/callable.ts
@@ -3,13 +3,13 @@ import * as functions from 'firebase-functions';
 import { NotebookEditorInsertText_Rest, NotebookEditorInsertText_Rest_Schema } from '@ureeka-notebook/service-common';
 
 import { wrapCall, SmallMemory } from '../util/function';
-import { insertText } from './commands';
+import { insertText } from './command';
 
 // ********************************************************************************
 // == Session =====================================================================
-// clears the specified User-Session
+// inserts the given text at the start of the Notebook.
 export const notebookEditorInsertText = functions.runWith(SmallMemory).https.onCall(wrapCall<NotebookEditorInsertText_Rest>(
-{ name: 'authUserSessionClear', schema: NotebookEditorInsertText_Rest_Schema, requiresAuth: true },
+{ name: 'notebookEditorInsertText', schema: NotebookEditorInsertText_Rest_Schema, requiresAuth: true },
 async (data, context, userId) => {
   await insertText(userId!/*auth'd*/, data.notebookId, data.text)/*throws on error*/;
 }));
