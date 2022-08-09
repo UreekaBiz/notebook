6778fef0b275656f9114fcdde1edad2af519cffb
Rob Grzywinski <rob.grzywinski@ureeka.biz>
2022-08-09 14:15:22 -0500

    Unused

--- packages/service-common/src/notebook/util.ts
+++ packages/service-common/src/notebook/util.ts
@@ -14,19 +14,3 @@ export const getNotebookShareRoles = (notebook: Notebook): Map<UserIdentifier, S
 
   return userRoles;
 };
-
-// --------------------------------------------------------------------------------
-// return `true` if the two maps are contain the same Users and Roles
-export const areNotebookShareRolesEqual = (a: Map<UserIdentifier, ShareRole>, b: Map<UserIdentifier, ShareRole>): boolean => {
-  // have the same number of entries
-  if(a.size !== b.size) return false;
-
-  // check each entry
-  // NOTE: using 'for' instead of 'forEach' for performance
-  for(const [userId, role] of a) {
-    if(!b.has(userId) || b.get(userId) !== role) return false/*differs -- early exit*/;
-  }
-
-  // all entries are equal
-  return true;
-};
