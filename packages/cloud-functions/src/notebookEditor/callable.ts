4701f5ffb079733d58fb93162acad455cc24023e
ErickJoestar <erickborquez@gmail.com>
2022-08-09 16:50:38 -0500

    Refacrtor

--- packages/cloud-functions/src/notebookEditor/callable.ts
+++ packages/cloud-functions/src/notebookEditor/callable.ts
@@ -3,7 +3,7 @@ import * as functions from 'firebase-functions';
 import { NotebookEditorInsertNumbers_Rest, NotebookEditorInsertNumbers_Rest_Schema, NotebookEditorInsertText_Rest, NotebookEditorInsertText_Rest_Schema } from '@ureeka-notebook/service-common';
 
 import { wrapCall, SmallMemory } from '../util/function';
-import { insertNumbers, insertText } from './command';
+import { insertNumbers, insertText, wrapCommandFunction } from './command';
 
 // ********************************************************************************
 // == Session =====================================================================
@@ -11,12 +11,13 @@ import { insertNumbers, insertText } from './command';
 export const notebookEditorInsertNumbers = functions.runWith(SmallMemory).https.onCall(wrapCall<NotebookEditorInsertNumbers_Rest>(
 { name: 'notebookEditorInsertNumbers', schema: NotebookEditorInsertNumbers_Rest_Schema, requiresAuth: true },
 async (data, context, userId) => {
-  await insertNumbers(userId!/*auth'd*/, data.notebookId)/*throws on error*/;
+  await wrapCommandFunction(userId!/*auth'd*/, data.notebookId, 'insertNumbers', insertNumbers())/*throws on error*/;
+
 }));
 
 // inserts the given text at the start of the Notebook.
 export const notebookEditorInsertText = functions.runWith(SmallMemory).https.onCall(wrapCall<NotebookEditorInsertText_Rest>(
 { name: 'notebookEditorInsertText', schema: NotebookEditorInsertText_Rest_Schema, requiresAuth: true },
 async (data, context, userId) => {
-  await insertText(userId!/*auth'd*/, data.notebookId, data.text)/*throws on error*/;
+  await wrapCommandFunction(userId!/*auth'd*/, data.notebookId, 'insertText', insertText(data.text))/*throws on error*/;
 }));
--- packages/cloud-functions/src/notebookEditor/command.ts
+++ packages/cloud-functions/src/notebookEditor/command.ts
@@ -1,31 +1,42 @@
-import { getEditorState, getRandomSystemUserId, Command, NotebookIdentifier, UserIdentifier, NO_NOTEBOOK_VERSION } from '@ureeka-notebook/service-common';
+import { EditorState } from 'prosemirror-state';
 
-import { firestore } from '../firebase';
+import { getEditorState, getRandomSystemUserId, sleep,  ClientIdentifier, Command, NotebookIdentifier, UserIdentifier, NO_NOTEBOOK_VERSION } from '@ureeka-notebook/service-common';
+
 import { notebookDocument } from '../notebook/datastore';
 import { ApplicationError } from '../util/error';
 import { getSnapshot } from '../util/firestore';
 import { getNotebookContent } from './checkpoint';
-import { saveEditorStateTransaction } from './transaction';
-import { getLastVersion } from './version';
+import { getLastVersion, writeVersions } from './version';
 
 // ********************************************************************************
-// FIXME: Both commands are really similar, they only differ in the command that is
-//        executed!
-// == Command =====================================================================
-// inserts multiple numbers at random positions in the Notebook.
-export const insertNumbers = async (
-  userId: UserIdentifier,
-  notebookId: NotebookIdentifier
-): Promise<NotebookIdentifier> => {
+// == Type ========================================================================
+type CollaborationDelay = Readonly<{
+  /** time in millis to delay before reading. No delay if <= 0 */
+  readDelayMs: number;
+  /** time in millis to delay before writing. No delay if <= 0 */
+  writeDelayMs: number;
+}>;
+const collaborationDelay: CollaborationDelay = { readDelayMs: 0, writeDelayMs: 0 };
+
+type CommandGenerator = (props: {
+  userId: UserIdentifier;
+  notebookId: NotebookIdentifier;
+  clientId: ClientIdentifier;
+  versionIndex: number;
+  editorState: EditorState;
+}) => Promise<Command>;
+
+// == Utility =====================================================================
+export const wrapCommandFunction = async (userId: UserIdentifier, notebookId: NotebookIdentifier, label: string, func: CommandGenerator ): Promise<NotebookIdentifier> => {
   try {
     // ensure that the Notebook document still exists (i.e. has not been deleted
     // either hard or soft) and that the caller has the right permissions to edit
     // its content.
     const notebookRef = notebookDocument(notebookId), notebookSnapshot = await getSnapshot(undefined/*no transaction*/, notebookRef);
-    if(!notebookSnapshot.exists) throw new ApplicationError('functions/not-found', `Cannot perform command insertNumbers for non-existing Notebook (${notebookId}) for User (${userId}).`);
+    if(!notebookSnapshot.exists) throw new ApplicationError('functions/not-found', `Cannot perform command ${label} for non-existing Notebook (${notebookId}) for User (${userId}).`);
     const notebook = notebookSnapshot.data()!;
-    if(notebook.deleted) throw new ApplicationError('data/deleted', `Cannot perform command insertNumbers for soft-deleted Notebook (${notebookId}) for User (${userId}).`);
-    if(!notebook.editors.includes(userId) && notebook.createdBy !== userId) throw new ApplicationError('functions/permission-denied', `Only Editors of a Notebook (${notebookId}) may perform command insertNumbers for User (${userId}).`);
+    if(notebook.deleted) throw new ApplicationError('data/deleted', `Cannot perform command ${label} for soft-deleted Notebook (${notebookId}) for User (${userId}).`);
+    if(!notebook.editors.includes(userId) && notebook.createdBy !== userId) throw new ApplicationError('functions/permission-denied', `Only Editors of a Notebook (${notebookId}) may perform command ${label} for User (${userId}).`);
 
     // gets the last version of the Notebook and gets the reference for the next
     // logical version. If no version exists then the next version is the first
@@ -34,93 +45,57 @@ export const insertNumbers = async (
     const nextVersionIndex = lastVersionIndex ? lastVersionIndex + 1 : NO_NOTEBOOK_VERSION/*start of document if no last version*/;
 
     // gets the content at the given version if it exists.
+    if(collaborationDelay.readDelayMs > 0) await sleep(collaborationDelay.writeDelayMs);
     const notebookContent = lastVersionIndex ? await getNotebookContent(undefined/*no transaction*/, notebook.schemaVersion, notebookId, lastVersionIndex) : undefined/*no content*/;
     const editorState = getEditorState(notebook.schemaVersion, notebookContent);
     if(!editorState) throw new ApplicationError('data/integrity', `Cannot create editorState for Notebook (${notebookId}) for version (${lastVersion}).`);
 
-    const command: Command = (tr) => {
-      // inserts 10 characters at random positions in the document.
-      for(let i=0;i<10;i++) {
-        const position = Math.floor(Math.random() * tr.doc.content.size) + 1/*start of valid content*/;
-        tr.insertText(String(i), position, position);
-      }
-      return true/*command can be performed*/;
-    };
-
     // Creates a unique identifier for the clientId.
+    // FIXME: Consistency
     const clientId = getRandomSystemUserId();
-    await saveEditorStateTransaction(
+
+    const command = await func({ clientId, editorState, notebookId, userId, versionIndex: nextVersionIndex });
+    // create a new transaction
+    const tr = editorState.tr;
+    // execute the command in the transaction
+    command(tr);
+
+    if(collaborationDelay.writeDelayMs > 0) await sleep(collaborationDelay.writeDelayMs);
+    // TODO: Implement system to get new steps and sync in case of failure.
+    // write the versions from the steps generated on the command
+    await writeVersions(
       notebookId,
       notebook.schemaVersion/*matching Notebook for consistency*/,
       userId,
       clientId,
       nextVersionIndex,
-      editorState,
-      command
+      tr.steps
     );
   } catch(error) {
     if(error instanceof ApplicationError) throw error;
-    throw new ApplicationError('datastore/write', `Error performing command insertNumbers for notebook (${notebookId}) for User (${userId}). Reason: `, error);
+    throw new ApplicationError('datastore/write', `Error performing command ${label} for notebook (${notebookId}) for User (${userId}). Reason: `, error);
   }
 
   return notebookId;
 };
 
+// == Command =====================================================================
+// inserts multiple numbers at random positions in the Notebook.
+export const insertNumbers = (): CommandGenerator => async () => {
+  return (tr) => {
+    // inserts 10 characters at random positions in the document.
+    for(let i=0;i<10;i++) {
+      const position = Math.floor(Math.random() * tr.doc.content.size) + 1/*start of valid content*/;
+      tr.insertText(String(i), position, position);
+    }
+    return true/*command can be performed*/;
+  };
+};
+
 // inserts the specified text at the start of the the specified notebook.
-export const insertText = async (
-  userId: UserIdentifier,
-  notebookId: NotebookIdentifier,
-  text: string
-): Promise<NotebookIdentifier> => {
-  try {
-    // FIXME: This may not need to be a transaction. saveEditorStateTransaction
-    //        creates a new transaction when saving the steps instead of using the
-    //        existing transaction to prevent getting the editor content multiple
-    //        times. Since both are transactions that can run multiple times the
-    //        amount of operations is exponentially larger.
-    await firestore.runTransaction(async transaction => {
-      // ensure that the Notebook document still exists (i.e. has not been deleted
-      // either hard or soft) and that the caller has the right permissions to edit
-      // its content.
-      const notebookRef = notebookDocument(notebookId);
-      const notebookSnapshot = await transaction.get(notebookRef);
-      if(!notebookSnapshot.exists) throw new ApplicationError('functions/not-found', `Cannot perform command insertText for non-existing Notebook (${notebookId}) for User (${userId}).`);
-      const notebook = notebookSnapshot.data()!;
-      if(notebook.deleted) throw new ApplicationError('data/deleted', `Cannot perform command insertText for soft-deleted Notebook (${notebookId}) for User (${userId}).`);
-      if(!notebook.editors.includes(userId) && notebook.createdBy !== userId) throw new ApplicationError('functions/permission-denied', `Only Editors of a Notebook (${notebookId}) may perform command insertText for User (${userId}).`);
-
-      // gets the last version of the Notebook and gets the reference for the next
-      // logical version. If no version exists then the next version is the first
-      const lastVersion = await getLastVersion(transaction, notebookId),
-            lastVersionIndex = lastVersion?.index;
-      const nextVersionIndex = lastVersionIndex ? lastVersionIndex + 1 : NO_NOTEBOOK_VERSION/*start of document if no last version*/;
-
-      // gets the content at the given version if it exists.
-      const notebookContent = lastVersionIndex ? await getNotebookContent(transaction, notebook.schemaVersion, notebookId, lastVersionIndex) : undefined/*no content*/;
-      const editorState = getEditorState(notebook.schemaVersion, notebookContent);
-      if(!editorState) throw new ApplicationError('data/integrity', `Cannot create editorState for Notebook (${notebookId}) for version (${lastVersion}).`);
-
-      const command: Command = (tr) => {
-        tr.insertText(text, 1, 1/*start of document*/);
-        return true/*command can be performed*/;
-      };
-
-      // Creates a unique identifier for the clientId.
-      const clientId = getRandomSystemUserId();
-      await saveEditorStateTransaction(
-        notebookId,
-        notebook.schemaVersion/*matching Notebook for consistency*/,
-        userId,
-        clientId,
-        nextVersionIndex,
-        editorState,
-        command
-      );
-    });
-  } catch(error) {
-    if(error instanceof ApplicationError) throw error;
-    throw new ApplicationError('datastore/write', `Error performing command insertText for notebook (${notebookId}) for User (${userId}). Reason: `, error);
-  }
-
-  return notebookId;
+export const insertText = (text: string): CommandGenerator =>  async () => {
+  return (tr) => {
+    tr.insertText(text, 1, 1/*start of document*/);
+    return true/*command can be performed*/;
+  };
 };
