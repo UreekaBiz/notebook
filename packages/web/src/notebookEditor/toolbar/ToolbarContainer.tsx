a860c6d141e9765f8241d8890aa288a0b5e789f0
Alan Rodz <s061199@hotmail.com>
2022-08-10 01:38:24 -0500

    match pm-devtool

--- packages/web/src/notebookEditor/toolbar/ToolbarContainer.tsx
+++ packages/web/src/notebookEditor/toolbar/ToolbarContainer.tsx
@@ -4,7 +4,7 @@ import { Flex, FlexProps, Text } from '@chakra-ui/react';
 interface Props extends FlexProps { name: string; }
 export const ToolContainer: React.FC<Props> = ({ name, width, marginTop, children }) =>
   <Flex flexDir='column' width={width} marginTop={marginTop}>
-    <Text fontSize='14px'>{name}</Text>
+    <Text fontSize='16px'>{name}</Text>
     <Flex gap={1}>
       {children}
     </Flex>
