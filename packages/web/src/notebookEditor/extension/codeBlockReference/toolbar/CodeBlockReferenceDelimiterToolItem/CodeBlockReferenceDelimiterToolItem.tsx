6f43891258e7951cba4339e3ed691e025b74785a
Alan Rodz <s061199@hotmail.com>
2022-08-11 10:24:12 -0500

    add CBR toolbar

---
+++ packages/web/src/notebookEditor/extension/codeBlockReference/toolbar/CodeBlockReferenceDelimiterToolItem/CodeBlockReferenceDelimiterToolItem.tsx
@@ -0,0 +1,82 @@
+import { Box, Flex } from '@chakra-ui/react';
+
+import { isCodeBlockReferenceNode, AttributeType, NodeName, InvalidMergedAttributeValue } from '@ureeka-notebook/web-service';
+
+import { isNodeSelection } from 'notebookEditor/extension/util/node';
+import { EditorToolComponentProps } from 'notebookEditor/toolbar/type';
+import { useLocalValue } from 'notebookEditor/shared/hook/useLocalValue';
+import { separateUnitFromString } from 'notebookEditor/theme/type';
+
+import { DelimiterToolItem } from './DelimiterToolItem';
+import { getTextDOMRenderedValue } from 'notebookEditor/extension/util/attribute';
+
+// ********************************************************************************
+// == Interface ===================================================================
+interface Props extends EditorToolComponentProps {/*no additional*/ }
+
+export interface CodeBlockReferenceDelimiterToolItemProps {
+  localValue: string;
+  inputPlaceholder: string;
+  commitChange:  (value?: string | undefined, focus?: boolean | undefined) => void;
+  resetLocalValue: () => void;
+  updateLocalValue: (newValue: string) => void;
+}
+
+// == Component ===================================================================
+export const CodeBlockReferenceDelimiterToolItem: React.FC<Props> = ({ editor, depth }) => {
+  const { selection } = editor.state;
+  if(!isNodeSelection(selection) || !isCodeBlockReferenceNode(selection.node)) throw new Error('Invalid CodeBlockReferenceLeftDelimiterToolItem  Render');
+
+  const leftDelimiter = getTextDOMRenderedValue(editor, AttributeType.LeftDelimiter) ?? ''/*default*/,
+        rightDelimiter = getTextDOMRenderedValue(editor, AttributeType.RightDelimiter) ?? ''/*default*/;
+  const leftDelimiterValue = leftDelimiter === InvalidMergedAttributeValue ? '' : leftDelimiter,
+        rightDelimiterValue = rightDelimiter === InvalidMergedAttributeValue ? '' : rightDelimiter;
+
+  // == Handler ===================================================================
+  // NOTE: Handler above State to prevent 'before declaration' error
+  const handleDelimiterChange = (delimiter: 'left' | 'right', value: string, focusEditor?: boolean) => {
+    const newDelimiterAttributeObj = delimiter === 'left'
+      ? ({ [AttributeType.LeftDelimiter]: value })
+      : ({ [AttributeType.RightDelimiter]: value });
+
+    editor.chain()
+      .updateAttributes(NodeName.CODEBLOCK_REFERENCE, newDelimiterAttributeObj)
+      .setNodeSelection(selection.$anchor.pos)
+      .run();
+
+    // focus the editor again
+    if(focusEditor) editor.commands.focus();
+  };
+
+  // == State =====================================================================
+  const { commitChange: commitLeftDelimiterChange, localValue: localLeftDelimiterValue, resetLocalValue: resetLocalLeftDelimiterValue, updateLocalValue: updateLocalLeftDelimiterValue } =
+    useLocalValue<string>(leftDelimiterValue, (newValue, focus) => handleDelimiterChange('left', newValue, focus));
+  let [separatedLeftValue] = separateUnitFromString(localLeftDelimiterValue);
+
+  const { commitChange: commitRightDelimiterChange, localValue: localRightDelimiterValue, resetLocalValue: resetLocalRightDelimiterValue, updateLocalValue: updateLocalRightDelimiterValue } =
+    useLocalValue<string>(rightDelimiterValue, (newValue, focus) => handleDelimiterChange('right', newValue, focus));
+  let [separatedRightValue] = separateUnitFromString(localRightDelimiterValue);
+
+  // == UI ========================================================================
+  return (
+    <Box>
+      Delimiters
+      <Flex marginTop='5px' gap='20px'>
+        <DelimiterToolItem
+          localValue={separatedLeftValue}
+          inputPlaceholder='Left Delimiter'
+          commitChange={commitLeftDelimiterChange}
+          resetLocalValue={resetLocalLeftDelimiterValue}
+          updateLocalValue={updateLocalLeftDelimiterValue}
+        />
+        <DelimiterToolItem
+          localValue={separatedRightValue}
+          inputPlaceholder='Right Delimiter'
+          commitChange={commitRightDelimiterChange}
+          resetLocalValue={resetLocalRightDelimiterValue}
+          updateLocalValue={updateLocalRightDelimiterValue}
+        />
+      </Flex>
+    </Box>
+  );
+};
---
+++ packages/web/src/notebookEditor/extension/codeBlockReference/toolbar/CodeBlockReferenceDelimiterToolItem/DelimiterToolItem.tsx
@@ -0,0 +1,42 @@
+import { Input } from '@chakra-ui/react';
+import { ChangeEventHandler, KeyboardEventHandler } from 'react';
+
+import { TOOL_ITEM_DATA_TYPE } from 'notebookEditor/toolbar/type';
+
+import { CodeBlockReferenceDelimiterToolItemProps } from './CodeBlockReferenceDelimiterToolItem';
+
+// ********************************************************************************
+export const DelimiterToolItem: React.FC<CodeBlockReferenceDelimiterToolItemProps> = ({ localValue, inputPlaceholder, commitChange, resetLocalValue, updateLocalValue }) => {
+// == Handler ===================================================================
+  const saveLocalValueChange = (focus: boolean = true) => {
+    if(localValue) commitChange(undefined/*use stored value*/, focus);
+    else resetLocalValue();
+  };
+
+  const handleLocalValueChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    const newValue = event.target.value;
+    updateLocalValue(`${newValue}`);
+  };
+
+  const handleKeyDown: KeyboardEventHandler<HTMLInputElement> = (event) => {
+    // save changes when user presses Enter
+    if(event.key === 'Enter') {
+      saveLocalValueChange();
+    } /* else -- ignore */
+  };
+
+  // == UI ========================================================================
+  return (
+    <Input
+      value={localValue}
+      size='sm'
+      autoComplete='off'
+      marginBottom='5px'
+      placeholder={inputPlaceholder}
+      datatype={TOOL_ITEM_DATA_TYPE/*(SEE: notebookEditor/toolbar/type )*/}
+      onBlur={() => saveLocalValueChange(false)}
+      onChange={handleLocalValueChange}
+      onKeyDown={handleKeyDown}
+    />
+  );
+};
