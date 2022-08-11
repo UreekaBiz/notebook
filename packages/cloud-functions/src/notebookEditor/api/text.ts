2b619038ad506c52acbd32492a249b02ca657772
Rob Grzywinski <rob.grzywinski@ureeka.biz>
2022-08-11 10:26:59 -0500

    Mis-named

--- packages/cloud-functions/src/notebookEditor/api/text.ts
+++ packages/cloud-functions/src/notebookEditor/api/text.ts
@@ -9,7 +9,7 @@ export class InsertText implements DocumentUpdate {
   public constructor(private readonly text: string, private readonly from?: number, private readonly to?: number) {/*nothing additional*/}
 
   // == DocumentUpdate ============================================================
-  public update(document: EditorState) {
-    return document.tr.insertText(this.text, this.from, this.to);
+  public update({ tr }: EditorState) {
+    return tr.insertText(this.text, this.from, this.to);
   }
 }
--- packages/cloud-functions/src/notebookEditor/api/type.ts
+++ packages/cloud-functions/src/notebookEditor/api/type.ts
@@ -3,5 +3,5 @@ import { EditorState } from 'prosemirror-state';
 // ********************************************************************************
 export type DocumentUpdate = Readonly<{
   /** modifies the specified ProseMirror Document */
-  update: (document: EditorState) => void;
+  update: (state: EditorState) => void;
 }>;
