bcea21a23d34d2bda320fa667d1f113011aec263
Alan Rodz <s061199@hotmail.com>
2022-08-11 10:19:20 -0500

    add CBR MVC

---
+++ packages/web/src/notebookEditor/extension/codeBlockReference/nodeView/model.ts
@@ -0,0 +1,10 @@
+import { CodeBlockReferenceNodeType } from '@ureeka-notebook/web-service';
+
+import { AbstractNodeModel } from 'notebookEditor/model/AbstractNodeModel';
+
+import { CodeBlockReferenceStorageType } from './controller';
+
+// == View ========================================================================
+export class CodeBlockReferenceModel extends AbstractNodeModel<CodeBlockReferenceNodeType, CodeBlockReferenceStorageType> {
+  // no need to implement anything here
+}
