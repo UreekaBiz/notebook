f41fff2f9dd9343c4a7586f59d89091f650054e0
Rob Grzywinski <rob.grzywinski@ureeka.biz>
2022-08-11 10:25:15 -0500

    Limited visibility for sanity

--- packages/cloud-functions/src/notebookEditor/api/text.ts
+++ packages/cloud-functions/src/notebookEditor/api/text.ts
@@ -6,7 +6,7 @@ import { DocumentUpdate } from './type';
 /** Inserts the specified text in the specified (optional) range
  *  @see Transaction#insertText() */
 export class InsertText implements DocumentUpdate {
-  public constructor(public readonly text: string, public readonly from?: number, public readonly to?: number) {/*nothing additional*/}
+  public constructor(private readonly text: string, private readonly from?: number, private readonly to?: number) {/*nothing additional*/}
 
   // == DocumentUpdate ============================================================
   public update(document: EditorState) {
