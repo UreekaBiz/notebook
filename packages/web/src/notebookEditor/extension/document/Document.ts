44ba6d5af96562a902e589f5066bb2a28202b976
Alan Rodz <s061199@hotmail.com>
2022-08-11 15:18:06 -0500

    match pm-devtool

--- packages/web/src/notebookEditor/extension/document/Document.ts
+++ packages/web/src/notebookEditor/extension/document/Document.ts
@@ -1,6 +1,8 @@
 import { Node } from '@tiptap/core';
+import { Plugin } from 'prosemirror-state';
+import { EditorView } from 'prosemirror-view';
 
-import { DocumentNodeSpec } from '@ureeka-notebook/web-service';
+import { DocumentNodeSpec, NotebookSchemaType } from '@ureeka-notebook/web-service';
 
 import { NoOptions, NoStorage } from 'notebookEditor/model/type';
 
@@ -10,4 +12,23 @@ import { NoOptions, NoStorage } from 'notebookEditor/model/type';
 // == Node ========================================================================
 export const Document = Node.create<NoOptions, NoStorage>({
   ...DocumentNodeSpec,
+
+  // -- Plugin --------------------------------------------------------------------
+  // REF: https://discuss.prosemirror.net/t/disable-ctrl-click/995/2
+  // NOTE: this Plugin prevents the default parent Node selection behavior
+  //      from being triggered when either the CMD or the CTRL keys are pressed.
+  //      This default behavior comes from PM (SEE: REF above)
+  addProseMirrorPlugins() {
+    return [
+      new Plugin<NotebookSchemaType>({
+        props: {
+          handleClick(view: EditorView, pos: number, event: MouseEvent) {
+            if(event.ctrlKey || event.metaKey) return true/*(SEE: comment above)*/;
+
+            return false/*allow regular event handling*/;
+          },
+        },
+      }),
+    ];
+  },
 });
