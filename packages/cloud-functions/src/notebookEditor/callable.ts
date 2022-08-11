9505ccba92b4e948b063ca653574b680212d6fcd
Rob Grzywinski <rob.grzywinski@ureeka.biz>
2022-08-11 13:20:32 -0500

    Server-side Demo Async Node SWAG (WIP)

--- packages/cloud-functions/src/notebookEditor/callable.ts
+++ packages/cloud-functions/src/notebookEditor/callable.ts
@@ -1,10 +1,11 @@
 import * as functions from 'firebase-functions';
 
-import { NotebookEditorInsertNumbers_Rest, NotebookEditorInsertNumbers_Rest_Schema, NotebookEditorInsertText_Rest, NotebookEditorInsertText_Rest_Schema } from '@ureeka-notebook/service-common';
+import { NotebookEditorDemoAsyncNodeExecute_Rest, NotebookEditorDemoAsyncNodeExecute_Rest_Schema, NotebookEditorInsertNumbers_Rest, NotebookEditorInsertNumbers_Rest_Schema, NotebookEditorInsertText_Rest, NotebookEditorInsertText_Rest_Schema } from '@ureeka-notebook/service-common';
 
 import { wrapCall, SmallMemory } from '../util/function';
 import { getDocument, updateDocument } from './api/api';
 import { InsertText } from './api/text';
+import { executeDemoAsyncNode } from './demoAsyncNode';
 
 // ********************************************************************************
 // == Example Document Update =====================================================
@@ -20,12 +21,19 @@ async (data, context, userId) => {
       return new InsertText(String(i), position, position);
     });
 
-  await updateDocument(userId!/*auth'd*/, data.notebookId, updates, { versionIndex }/*constrain for example*/);
+  await updateDocument(userId!/*auth'd*/, data.notebookId, updates, { versionIndex }/*constrain for example*/)/*throws on error*/;
 }));
 
 // inserts the given text at the start of the Notebook
 export const notebookEditorInsertText = functions.runWith(SmallMemory).https.onCall(wrapCall<NotebookEditorInsertText_Rest>(
 { name: 'notebookEditorInsertText', schema: NotebookEditorInsertText_Rest_Schema, requiresAuth: true },
 async (data, context, userId) => {
-  await updateDocument(userId!/*auth'd*/, data.notebookId, [ new InsertText(data.text) ]);
+  await updateDocument(userId!/*auth'd*/, data.notebookId, [ new InsertText(data.text) ])/*throws on error*/;
+}));
+
+// == Demo Async Node =============================================================
+export const notebookEditorDemoAsyncNodeExecute = functions.runWith(SmallMemory).https.onCall(wrapCall<NotebookEditorDemoAsyncNodeExecute_Rest>(
+{ name: 'notebookEditorDemoAsyncNodeExecute', schema: NotebookEditorDemoAsyncNodeExecute_Rest_Schema, requiresAuth: true },
+async (data, context, userId) => {
+  await executeDemoAsyncNode(userId!/*auth'd*/, data.notebookId, data.nodeId, data.content)/*throws on error*/;
 }));
---
+++ packages/web-service/src/notebookEditor/type.ts
@@ -0,0 +1,6 @@
+import { NotebookEditorDemoAsyncNodeExecute_Rest, NotebookEditorDemoAsyncNodeExecute_Rest_Schema } from '@ureeka-notebook/service-common';
+
+// ********************************************************************************
+// == Notebook Editor =============================================================
+export const NotebookEditorDemoAsyncNode_Execute_Schema = NotebookEditorDemoAsyncNodeExecute_Rest_Schema;
+export type NotebookEditorDemoAsyncNode_Execute = NotebookEditorDemoAsyncNodeExecute_Rest;
