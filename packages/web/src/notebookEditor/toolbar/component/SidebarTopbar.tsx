a1c2c558f4f92c484303b0c5fc85eb38d75f4270
ErickJoestar <erickborquez@gmail.com>
2022-08-12 09:44:22 -0500

    comments

--- packages/web/src/notebookEditor/toolbar/component/SidebarTopbar.tsx
+++ packages/web/src/notebookEditor/toolbar/component/SidebarTopbar.tsx
@@ -33,11 +33,13 @@ export const SidebarTopbar: React.FC<Props> = ({ background }) => {
 
   // == Effect ====================================================================
   useEffect(() => {
-    // debounce setting isLoading to true to avoid flashing the text in each save.
+    // debounce setting setHasPendingWrite to true to avoid flashing the text in
+    // each save.
     const debounced = debounce((value: boolean) => setHasPendingWrite(value), 1000);
 
     const subscription = editorService.onPendingWrites$().subscribe({
       next: (hasPendingWrite) => {
+        // cancel previous call to setHasPendingWrite if it's still pending
         debounced(hasPendingWrite);
 
         // immediately set isLoading to false if there are no pending writes
