718e39166b8729ffbf7e4b690fe8b814172055d7
Alan Rodz <s061199@hotmail.com>
2022-08-12 12:18:49 -0500

    match pm-devtool

--- packages/web/src/notebookEditor/extension/heading/command.ts
+++ packages/web/src/notebookEditor/extension/heading/command.ts
@@ -1,4 +1,5 @@
 import { CommandProps } from '@tiptap/core';
+import { Selection } from 'prosemirror-state';
 
 import { createBoldMark, isHeadingLevel, AttributeType, CommandFunctionType, HeadingLevel, NodeName, MarkName } from '@ureeka-notebook/web-service';
 
@@ -19,8 +20,7 @@ declare module '@tiptap/core' {
 export const setHeadingCommand = (attributes: { level: HeadingLevel; }) => ({ editor, chain }: CommandProps) => {
   if(!isHeadingLevel(attributes[AttributeType.Level])) return false/*invalid command, level for heading not supported*/;
 
-  let shouldInsertMarkHolder = editor.state.selection.$anchor.parent.content.size < 1;
-  if(shouldInsertMarkHolder) {
+  if(shouldInsertMarkHolder(editor.state.selection)) {
     return chain().setNode(NodeName.HEADING, attributes)
                   .insertContent(createMarkHolderJSONNode(editor, [MarkName.BOLD]))
                   .run();
@@ -41,8 +41,7 @@ export const toggleHeadingCommand = (attributes: { level: HeadingLevel; }) => ({
     return chain().toggleNode(NodeName.PARAGRAPH, NodeName.HEADING, attributes).run();
   } /* else -- set heading normally */
 
-  let shouldInsertMarkHolder = editor.state.selection.$anchor.parent.content.size < 1;
-  if(shouldInsertMarkHolder) {
+  if(shouldInsertMarkHolder(editor.state.selection)) {
     return chain().toggleNode(NodeName.HEADING, NodeName.PARAGRAPH, attributes)
                   .insertContent(createMarkHolderJSONNode(editor, [MarkName.BOLD]))
                   .run();
@@ -54,18 +53,35 @@ export const toggleHeadingCommand = (attributes: { level: HeadingLevel; }) => ({
 };
 
 // == Util ========================================================================
-// applies the Bold Mark to the whole content of the parent of the selection
+// applies the Bold Mark to the whole content of the parents of the selection
 const applyBoldToHeadingContent = (props: CommandProps) => {
   const { editor, dispatch,  tr } = props;
   if(tr.selection.$anchor.parent.content.size < 0) return false/*command cannot be executed, the Heading has no content to apply the Bold Mark*/;
 
-  const currentPos = tr.selection.$anchor.pos,
-        offset = tr.selection.$anchor.parentOffset,
-        parentPos = currentPos - offset;
   if(dispatch) {
-    tr.addMark(parentPos, parentPos + tr.selection.$anchor.parent.nodeSize - 2/*account for the start and end of the parent Node*/, createBoldMark(editor.schema));
+    // apply Bold Mark to anchor parent Content
+    const currentAnchorPos = tr.selection.$anchor.pos;
+    const anchorOffset = tr.selection.$anchor.parentOffset,
+          anchorParentOffset = currentAnchorPos - anchorOffset;
+    tr.addMark(anchorParentOffset, anchorParentOffset + tr.selection.$anchor.parent.nodeSize - 2/*account for the start and end of the parent Node*/, createBoldMark(editor.schema));
+
+    // apply Bold Mark to head parent Content
+    const currentHeadPos = tr.selection.$head.pos;
+    const headOffset = tr.selection.$head.parentOffset,
+          headParentOffset = currentHeadPos - headOffset;
+    tr.addMark(headParentOffset, headParentOffset + tr.selection.$head.parent.nodeSize - 2/*account for the start and end of the parent Node*/, createBoldMark(editor.schema));
+
+    // apply Bold Mark to any nodes in between anchor and head
+    currentHeadPos > currentAnchorPos/*check which position is bigger to add Mark correctly*/
+      ? tr.addMark(currentAnchorPos, currentHeadPos, createBoldMark(editor.schema))
+      : tr.addMark(currentHeadPos, currentAnchorPos, createBoldMark(editor.schema));
+
     dispatch(tr);
   } /* else -- called from can() (SEE: src/notebookEditor/README.md/#Commands) */
 
   return true/*command can be executed*/;
 };
+
+// a MarkHolder should be inserted if the Selection is empty, the anchor and the
+// head are in the same place, and the parent of the selection has no content
+const shouldInsertMarkHolder = (selection: Selection) => selection.empty && selection.$anchor.pos === selection.$head.pos && selection.$anchor.parent.content.size < 1;
