6040dec6f907787b104ddcf9119f36cf7889e120
Alan Rodz <s061199@hotmail.com>
2022-08-13 00:27:26 -0500

    match pm devtool (use setBlockNodeAcrossNodes in D2AN)

--- packages/web/src/notebookEditor/extension/demo2AsyncNode/command.ts
+++ packages/web/src/notebookEditor/extension/demo2AsyncNode/command.ts
@@ -1,7 +1,6 @@
 import { CommandProps } from '@tiptap/core';
-import { TextSelection } from 'prosemirror-state';
 
-import { createTextNode, createDemo2AsyncNodeNode, getBlockNodeRange, generateNodeId, getParentNode, isDemo2AsyncNode, AttributeType, Command, CommandFunctionType, NodeName, NotebookSchemaType } from '@ureeka-notebook/web-service';
+import { generateNodeId, getParentNode, isDemo2AsyncNode, setBlockNodeAcrossNodes, AttributeType, CommandFunctionType, NodeName } from '@ureeka-notebook/web-service';
 
 // == Type ========================================================================
 // NOTE: Usage of ambient module to ensure command is TypeScript-registered
@@ -23,17 +22,5 @@ export const toggleDemo2AsyncNodeCommand = () => ({ editor, commands, tr, dispat
     return commands.setNode(NodeName.DEMO_2_ASYNC_NODE, { [AttributeType.Id]: generateNodeId() });
   } /* else -- ensure selected Content gets transformed into Demo2AsyncNode correctly */
 
-  return setDemo2AsyncNodeAcrossNodes(editor.schema)(tr, dispatch, view);
-};
-
-// --------------------------------------------------------------------------------
-const setDemo2AsyncNodeAcrossNodes = (schema: NotebookSchemaType): Command => (tr, dispatch, view) => {
-  const { from, to } = getBlockNodeRange(tr.selection);
-  const textContent = tr.doc.textBetween(from, to, '\n'/*insert for every Block Node*/);
-
-  tr.setSelection(new TextSelection(tr.doc.resolve(from - 1/*account for start of parent at from*/), tr.doc.resolve(to)))
-    .replaceSelectionWith(createDemo2AsyncNodeNode(schema, { [AttributeType.Id]: generateNodeId() }, createTextNode(schema, textContent)));
-
-  if(dispatch) dispatch(tr);
-  return true/*can be done*/;
+  return setBlockNodeAcrossNodes(editor.schema, NodeName.DEMO_2_ASYNC_NODE, { [AttributeType.Id]: generateNodeId() })(tr, dispatch, view);
 };
