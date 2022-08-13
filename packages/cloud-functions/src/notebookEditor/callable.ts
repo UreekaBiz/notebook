793090bd2605399419ca859c1a6a4d96911f70a1
Rob Grzywinski <rob.grzywinski@ureeka.biz>
2022-08-13 10:20:05 -0500

    Consistency

--- packages/cloud-functions/src/notebookEditor/callable.ts
+++ packages/cloud-functions/src/notebookEditor/callable.ts
@@ -32,16 +32,16 @@ async (data, context, userId) => {
   await updateDocument(userId!/*auth'd*/, data.notebookId, [ new InsertText(data.text) ])/*throws on error*/;
 }));
 
-// == Demo 2 Async Node =============================================================
-export const notebookEditorDemo2AsyncNodeExecute = functions.runWith(SmallMemory).https.onCall(wrapCall<NotebookEditorDemo2AsyncNodeExecute_Rest>(
-  { name: 'notebookEditorDemo2AsyncNodeExecute', schema: NotebookEditorDemo2AsyncNodeExecute_Rest_Schema, requiresAuth: true },
-  async (data, context, userId) => {
-    await executeDemo2AsyncNode(userId!/*auth'd*/, data.notebookId, data.nodeId, data.content, data.replace)/*throws on error*/;
-  }));
-
 // == Demo Async Node =============================================================
 export const notebookEditorDemoAsyncNodeExecute = functions.runWith(SmallMemory).https.onCall(wrapCall<NotebookEditorDemoAsyncNodeExecute_Rest>(
 { name: 'notebookEditorDemoAsyncNodeExecute', schema: NotebookEditorDemoAsyncNodeExecute_Rest_Schema, requiresAuth: true },
 async (data, context, userId) => {
   await executeDemoAsyncNode(userId!/*auth'd*/, data.notebookId, data.nodeId, data.hashes, data.content)/*throws on error*/;
+}));
+
+// == Demo 2 Async Node =============================================================
+export const notebookEditorDemo2AsyncNodeExecute = functions.runWith(SmallMemory).https.onCall(wrapCall<NotebookEditorDemo2AsyncNodeExecute_Rest>(
+{ name: 'notebookEditorDemo2AsyncNodeExecute', schema: NotebookEditorDemo2AsyncNodeExecute_Rest_Schema, requiresAuth: true },
+async (data, context, userId) => {
+  await executeDemo2AsyncNode(userId!/*auth'd*/, data.notebookId, data.nodeId, data.content, data.replace)/*throws on error*/;
 }));
