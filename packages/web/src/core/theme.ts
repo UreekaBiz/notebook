125a7471d2470091323be62c615134c3145147df
Alan Rodz <s061199@hotmail.com>
2022-08-17 16:45:31 -0500

    remove currently non used

--- packages/web/src/core/theme.ts
+++ packages/web/src/core/theme.ts
@@ -12,38 +12,5 @@ export const DEFAULT_H3_COLOR = '#89B181';
 // ================================================================================
 export const theme = extendTheme({
   fonts: { code:'source-code-pro, Menlo, Monaco, Consolas, Courier New, monospace' },
-
-  styles: {
-    global: {
-
-      // --- HTML Tags ------------------------------------------------------------
-      code: {
-        borderRadius:'5',
-        display: 'fit-content',
-        fontFamily: 'Menlo, Consolas, monospace',
-        fontSize:'0.845em' /*T&E. This value is meant to match the size of the default font*/,
-
-        /**
-         * NOTE: This minWidth makes it so that empty CodeBlockContents can be used and
-         * the cursor does not disappear when set inside of them (like paragraphs)
-         * This is a CSS fix for the following issue
-         *
-         * REF: https://github.com/ueberdosis/tiptap/issues/911
-         */
-        div: { minWidth: '1px' },
-      },
-
-      a: {
-        color: '#3b5fc0',
-        textDecoration: 'underline',
-        cursor: 'auto',
-      },
-
-      img: {
-        height: 'auto',
-        maxWidth: '100%',
-        maxHeight: '100%',
-      },
-    },
-  },
+  styles: { global: {/*currently nothing*/} },
 });
