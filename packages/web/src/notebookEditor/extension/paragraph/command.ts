bcdadb723f37bc7905141fcd32daeedef2dd7896
Alan Rodz <s061199@hotmail.com>
2022-08-16 21:56:18 -0500

    match pm-devtool (change setParagraph to work with PM only)

--- packages/web/src/notebookEditor/extension/paragraph/command.ts
+++ packages/web/src/notebookEditor/extension/paragraph/command.ts
@@ -1,6 +1,7 @@
 import { CommandProps } from '@tiptap/core';
+import { TextSelection } from 'prosemirror-state';
 
-import { getBlockNodeRange, CommandFunctionType, NodeName } from '@ureeka-notebook/web-service';
+import { createParagraphNode, getBlockNodeRange, CommandFunctionType, NodeName } from '@ureeka-notebook/web-service';
 
 // ********************************************************************************
 // NOTE: ambient module to ensure command is TypeScript-registered for TipTap
@@ -14,10 +15,19 @@ declare module '@tiptap/core' {
 }
 
 // --------------------------------------------------------------------------------
-export const setParagraphCommand = () => ({ state, chain }: CommandProps) =>
-  chain()
-    .setTextSelection(getBlockNodeRange(state.selection))
-    .unsetAllMarks()
-    .setNode(NodeName.PARAGRAPH)
-    .setTextSelection(state.selection.$anchor.pos)
-    .run();
+export const setParagraphCommand = () => ({ tr, dispatch, view }: CommandProps) => ((tr, dispatch, view) => {
+  const { from, to } = getBlockNodeRange(tr.selection);
+  const textContent = tr.doc.textBetween(from, to, '\n');
+  const resolvedFrom = tr.doc.resolve(from - 1/*select the whole parent*/),
+        resolvedTo = tr.doc.resolve(to + 1/*select the whole parent*/);
+
+  // NOTE: this is specifically replacing the selection instead of calling
+  //       setBlockType to account for the case where the Paragraph's only
+  //       Content is a MarkHolder Node, which gets deleted by the
+  //       command (effectively removing Marks on next input)
+  tr.setSelection(new TextSelection(resolvedFrom, resolvedTo))
+    .replaceSelectionWith(createParagraphNode(view?.state.schema, undefined/*do not specify attrs*/, textContent.length > 0 ? view?.state.schema.text(textContent) : undefined/*no content*/));
+
+  if(dispatch) dispatch(tr);
+  return true/*can be executed*/;
+})(tr, dispatch, view);
