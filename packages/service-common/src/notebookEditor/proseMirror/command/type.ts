8988054953b10fa88044f364b102b00840c47931
Alan Rodz <s061199@hotmail.com>
2022-08-22 15:17:45 -0500

    match pm-devtool command type

--- packages/service-common/src/notebookEditor/proseMirror/command/type.ts
+++ packages/service-common/src/notebookEditor/proseMirror/command/type.ts
@@ -1,5 +1,5 @@
-import { Transaction } from 'prosemirror-state';
+import { EditorState, Transaction } from 'prosemirror-state';
 import { EditorView } from 'prosemirror-view';
 
 // ********************************************************************************
-export type Command = (tr: Transaction, dispatch?: (tr: Transaction) => void, view?: EditorView) => boolean/*indicates whether the command can be performed*/;
+export type Command = (state: EditorState, dispatch: (tr: Transaction) => void, view: EditorView) => boolean/*indicates whether the command can be performed*/;
