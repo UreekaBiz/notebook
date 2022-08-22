bc107266f777d63951dc1e6a540e879f51bfbecf
Alan Rodz <s061199@hotmail.com>
2022-08-22 15:18:02 -0500

    match pm-devtool (command util)

--- packages/web/src/notebookEditor/extension/codeblock/command.ts
+++ packages/web/src/notebookEditor/extension/codeblock/command.ts
@@ -1,24 +1,12 @@
-import { CommandProps } from '@tiptap/core';
+import { Editor } from '@tiptap/core';
 
-import { createBlockNodeBelow, generateNodeId, getParentNode, isCodeBlockNode, AttributeType, CommandFunctionType, NodeName } from '@ureeka-notebook/web-service';
+import { Command } from '@ureeka-notebook/web-service';
 
 // ********************************************************************************
-// == Type ========================================================================
-// NOTE: Usage of ambient module to ensure command is TypeScript-registered
-declare module '@tiptap/core' {
-  interface Commands<ReturnType> {
-    [NodeName.CODEBLOCK/*Expected and guaranteed to be unique. (SEE: /notebookEditor/model/node)*/]: {
-      /** Toggle a code block */
-      toggleCodeBlock: CommandFunctionType<typeof toggleCodeBlockCommand, ReturnType>;
-    };
-  }
-}
+/** Executes the given {@link Command} with the current {@link Editor} props */
+export const shortcutCommandWrapper = (editor: Editor, command: Command) => {
+  const { state, view } = editor;
+  const { dispatch } = view;
 
-// --------------------------------------------------------------------------------
-export const toggleCodeBlockCommand = () => ({ editor, commands, tr, dispatch, view }: CommandProps) => {
-  if(isCodeBlockNode(getParentNode(editor.state.selection))) {
-    return false/*do not allow codeBlocks to be toggable*/;
-  } /* else -- create a codeBlock */
-
-  return createBlockNodeBelow(editor.schema, NodeName.CODEBLOCK, { [AttributeType.Id]: generateNodeId() })(tr, dispatch, view);
+  return command(state, dispatch, view);
 };
