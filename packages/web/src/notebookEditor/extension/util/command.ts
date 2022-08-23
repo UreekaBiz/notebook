2f79b4dae1d4343c01841ff2cfbb6ff131de448b
Alan Rodz <s061199@hotmail.com>
2022-08-22 23:57:27 -0500

    match pm-devtool (add util)

--- packages/web/src/notebookEditor/extension/util/command.ts
+++ packages/web/src/notebookEditor/extension/util/command.ts
@@ -1,12 +1,29 @@
 import { Editor } from '@tiptap/core';
 
-import { Command } from '@ureeka-notebook/web-service';
+import { Command, SelectionDepth } from '@ureeka-notebook/web-service';
 
 // ********************************************************************************
-/** Executes the given {@link Command} with the current {@link Editor} props */
+/**
+ * Executes the given {@link Command} with the current {@link Editor} props.
+ * Used by Keyboard Shortcuts
+ */
 export const shortcutCommandWrapper = (editor: Editor, command: Command) => {
-  const { state, view } = editor;
-  const { dispatch } = view;
-
+  const { state, view, dispatch } = getCommandPropsFromEditor(editor);
   return command(state, dispatch, view);
 };
+
+/**
+ * Executes the given {@link Command} with the current {@link Editor} props.
+ * Used by ToolItems
+ */
+ export const toolItemCommandWrapper = (editor: Editor, depth: SelectionDepth, command: Command) => {
+  const { state, view, dispatch } = getCommandPropsFromEditor(editor);
+  return command(state, dispatch, view);
+};
+
+/** Returns the required props to execute a {@link Command} from a given {@link Editor} */
+const getCommandPropsFromEditor = (editor: Editor) => ({
+  state: editor.state,
+  view: editor.view,
+  dispatch: editor.view.dispatch,
+});
