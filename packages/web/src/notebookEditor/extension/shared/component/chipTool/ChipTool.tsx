424109608e89416534f85fdc354fa92a9c46c4f0
ErickJoestar <erickborquez@gmail.com>
2022-08-23 15:07:39 -0500

    Refactor ChipToolItem

--- packages/web/src/notebookEditor/extension/shared/component/chipTool/ChipTool.tsx
+++ packages/web/src/notebookEditor/extension/shared/component/chipTool/ChipTool.tsx
@@ -3,79 +3,131 @@ import { useState } from 'react';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 
-import { ToolContainer } from 'notebookEditor/toolbar/ToolbarContainer';
+import { getLogger, Logger } from '@ureeka-notebook/web-service';
+
 import { TOOL_ITEM_DATA_TYPE } from 'notebookEditor/toolbar/type';
 
-import { ChipContainer } from './ChipContainer';
-import { ChipDraggableItem } from './Chip';
+import { Chip, ChipValue } from './Chip';
+
+const log = getLogger(Logger.NOTEBOOK);
 
 // ********************************************************************************
-// == Constant ====================================================================
 export const CHIP_TOOL_INPUT = 'chipToolInput';
 
-// == Interface ===================================================================
 interface Props {
-  nodeId: string;
-  name: string;
-  width: string;
-  marginTop: string;
-  currentChips: string[];
-  updateChipsInputCallback: (addedChipValue: string) => boolean;
-  chipClickCallback: (chipText: string) => void;
-  chipDropCallback: (item: ChipDraggableItem) => void;
-  chipCloseButtonCallback: (deletedIndex: number) => void;
+  /** the id of Node */
+  nodeId?: string;
+
+  /** the actual value of the tool. */
+  value: ChipValue[];
+  /** the maximum amount of allowed values */
+  maxValues?: number;
+
+  /** can ChipValues be duplicated? */
+  allowDuplicates?: boolean;
+
+  /** are chips draggable? */
+  isDraggable?: boolean;
+
+  /** callback to be called to validate if the chip label is valid.*/
+  validate: (label: string) => boolean;
+  onAddValue: (label: string, focus?: boolean) => void;
+  onChange: (value: ChipValue[], focus?: boolean) => void;
+  onChipClick?: (chip: ChipValue, index: number) => void;
 }
 
-// == Component ===================================================================
-export const ChipTool: React.FC<Props> = ({ nodeId, name, width, marginTop, currentChips, updateChipsInputCallback, chipClickCallback, chipDropCallback, chipCloseButtonCallback }) => {
-  // -- State ---------------------------------------------------------------------
+export const ChipTool: React.FC<Props> = ({ nodeId, value, maxValues, isDraggable = true, allowDuplicates = false, validate, onAddValue, onChange, onChipClick }) => {
+  // == State =====================================================================
   const [inputValue, setInputValue] = useState('');
 
-  // -- Handler -------------------------------------------------------------------
+  // == Handler ===================================================================
+  // -- Input ---------------------------------------------------------------------
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => setInputValue(event.target.value);
+
   const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if((event.code === 'Tab' || event.code === 'Enter') && inputValue.length > 0/*user not trying to focus next toolItem*/) {
-      event.preventDefault();
-      event.stopPropagation();
+    if(event.code !== 'Tab' && event.code !== 'Enter') return/*don't handle*/;
+    if(inputValue.length < 1 || !validate(inputValue)) return/*invalid chip*/;
+    if(!allowDuplicates && value.find(chip => chip.label === inputValue)) return/*duplicated value -- don't create chip*/;
 
-      const chipsUpdated = updateChipsInputCallback(inputValue);
-      if(chipsUpdated) {
-        setInputValue('');
-      } /* else -- do not reset inputValue */
+    event.preventDefault();
+    event.stopPropagation();
+    // update the input value before calling the callback
+    setInputValue('');
 
-      return/*work done*/;
-    } /* else -- handle keydown regularly */
+    if(maxValues !== undefined && maxValues >= 0 && value.length >= maxValues) return/*max values reached*/;
+    onAddValue(inputValue, event.code === 'Enter'/*focus on Enter*/);
   };
 
-  // -- UI ------------------------------------------------------------------------
+  // -- Chips ---------------------------------------------------------------------
+  const handleChipChange = (newValue: ChipValue[], focus: boolean) => {
+    // gets the amount of values in range
+    if(maxValues !== undefined){
+      if(newValue.length > maxValues) log.error('ChipTool: max values reached. Ignoring overflowing items.');
+      newValue = newValue.slice(0, maxValues);
+    }
+
+    onChange(newValue, true/*focus editor on chips change*/);
+  };
+
+  const handleMove = (from: number, to: number) => {
+    const newValue = [...value],
+          chip = newValue[from];
+
+    // remove the dragged chip from the array
+    newValue.splice(from, 1);
+    // insert chip at hover position
+    newValue.splice(to, 0, chip);
+    handleChipChange(newValue, false/*don't focus while moving*/);
+  };
+
+  // currently it does nothing
+  const handleDrop = (chip: ChipValue, index: number) => {
+    handleChipChange(value, false/*focus on dropping*/);
+  };
+
+  const handleDelete = (index: number) => {
+    const newValue = [...value];
+
+    // remove the dragged chip from the array
+    newValue.splice(index, 1);
+    handleChipChange(newValue, false/*don't focus while deleting*/);
+  };
+
+  // == UI ========================================================================
   return (
-    <ToolContainer name={name} width={width} marginTop={marginTop}>
-      <Flex
-        flexDir='column'
-        marginTop='5px'
-      >
-        <DndProvider backend={HTML5Backend}>
-          <ChipContainer
-            chipStringArray={currentChips}
-            chipClickCallback={chipClickCallback}
-            chipDropCallback={chipDropCallback}
-            chipCloseButtonCallback={chipCloseButtonCallback}
+    <DndProvider backend={HTML5Backend}>
+      <Flex flexDir='row' flexWrap='wrap' gap='1'>
+        {value.map((chip, index) => {
+          const key = `${chip.value}-${index}`;
+          return (
+            <Chip
+              key={key}
+              id={key}
+              value={chip}
+              index={index}
+              isDraggable={isDraggable}
+              onMove={handleMove}
+              onClick={() => onChipClick && onChipClick(chip, index)}
+              onDrop={(item) => handleDrop(chip, item.index)}
+              onClose={() => handleDelete(index)}
+            />
+          );
+        })}
+        {(maxValues === undefined || value.length < maxValues) && (
+          <Input
+            id={`${nodeId}-${CHIP_TOOL_INPUT}`}
+            size='sm'
+            autoComplete='off'
+            placeholder='CodeBlock Id'
+            value={inputValue}
+            variant='unstyled'
+            width='90px'
+            datatype={TOOL_ITEM_DATA_TYPE/*(SEE: notebookEditor/toolbar/type )*/}
+            onChange={handleInputChange}
+            onKeyDown={handleKeyDown}
           />
-        </DndProvider>
-        <Input
-          id={`${nodeId}-${CHIP_TOOL_INPUT}`}
-          flexBasis='70%'
-          size='sm'
-          marginTop='10px'
-          autoComplete='off'
-          placeholder='Add CodeBlock ID...'
-          value={inputValue}
-          variant='unstyled'
-          datatype={TOOL_ITEM_DATA_TYPE/*(SEE: notebookEditor/toolbar/type )*/}
-          onChange={handleInputChange}
-          onKeyDown={handleKeyDown}
-        />
+        )}
       </Flex>
-    </ToolContainer>
+    </DndProvider>
   );
 };
---
+++ packages/web/src/notebookEditor/extension/shared/component/chipTool/ChipToolItem.tsx
@@ -0,0 +1,70 @@
+import { getSelectedNode, isNodeSelection, isNodeType, AttributeType, NodeName } from '@ureeka-notebook/web-service';
+
+import { EditorToolComponentProps } from 'notebookEditor/toolbar/type';
+
+import { InputToolItemContainer } from '../InputToolItemContainer';
+import { ChipValue } from './Chip';
+import { ChipTool } from './ChipTool';
+
+// ********************************************************************************
+interface Props extends EditorToolComponentProps {
+  nodeName: NodeName;
+
+  /** the attribute that this ToolItems corresponds to */
+  attributeType: AttributeType;
+
+  /** the name of the ToolItem */
+  name: string;
+
+  /** a function that validates if a label is valid for the Chip */
+  validate: (label: string) => boolean;
+  /** gets a valid value from the given label */
+  getValueFromLabel: (label: string) => string;
+  /** gets a valid label from the given value */
+  getLabelFromValue: (value: string) => string;
+
+  onChipClick?: (chip: ChipValue, index: number) => void;
+}
+export const ChipToolItem: React.FC<Props> = ({ editor, attributeType, depth, name, nodeName, validate, getLabelFromValue, getValueFromLabel, onChipClick }) => {
+  const { state } = editor;
+  const { selection } = state;
+  const node = getSelectedNode(state, depth);
+  if(!node || !isNodeType(node, nodeName)) return null/*nothing to render - invalid node render*/;
+
+  const value = (node.attrs[attributeType] ?? [] /*default*/) as string[],
+        id = node.attrs[AttributeType.Id] ?? ''/*no id by default*/;
+  const chips = value.map(chip => ({ label: getLabelFromValue(chip), value: chip }));
+
+  // == Handler ===================================================================
+  const handleAddValue = (label: string, focus?: boolean) => {
+    const newValue: ChipValue = { label, value: getValueFromLabel(label) };
+    handleChange([...chips, newValue]);
+  };
+
+  const handleChange = (chips: ChipValue[], focus?: boolean) => {
+    const newValue = chips.map(chip => chip.value);
+    editor.commands.updateAttributes(nodeName, { [attributeType]: newValue });
+
+    const position = state.selection.$anchor.pos;
+    // set the selection in the same position in case that the node was replaced
+    if(isNodeSelection(selection)) editor.commands.setNodeSelection(position);
+    else editor.commands.setTextSelection(position);
+
+    // Focus the editor again
+    if(focus) editor.commands.focus();
+  };
+
+  // == UI ========================================================================
+  return (
+    <InputToolItemContainer name={name}>
+      <ChipTool
+        nodeId={id}
+        value={chips}
+        validate={validate}
+        onAddValue={handleAddValue}
+        onChange={handleChange}
+        onChipClick={onChipClick}
+      />
+    </InputToolItemContainer>
+  );
+};
