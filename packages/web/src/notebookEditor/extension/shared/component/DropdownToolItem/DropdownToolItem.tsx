5501aaafdba6b6d9b0201cf27134c026249ef7ba
ErickJoestar <erickborquez@gmail.com>
2022-08-22 20:17:40 -0500

    Refactor Dropdown tool item

--- packages/web/src/notebookEditor/extension/shared/component/DropdownToolItem/DropdownToolItem.tsx
+++ packages/web/src/notebookEditor/extension/shared/component/DropdownToolItem/DropdownToolItem.tsx
@@ -1,13 +1,11 @@
-import { Box, Flex, Text } from '@chakra-ui/react';
-
-import { getSelectedNode, isNodeType, AttributeType, NodeName } from '@ureeka-notebook/web-service';
+import { getSelectedNode, isNodeType, AttributeType, NodeName, isNodeSelection } from '@ureeka-notebook/web-service';
 
 import { EditorToolComponentProps } from 'notebookEditor/toolbar/type';
 
+import { InputToolItemContainer } from '../InputToolItemContainer';
 import { DropdownTool, DropdownToolItemType } from './DropdownTool';
 
 // ********************************************************************************
-// == Component ===================================================================
 interface Props extends EditorToolComponentProps {
   /** the NodeName of the Node */
   nodeName: NodeName;
@@ -21,15 +19,18 @@ interface Props extends EditorToolComponentProps {
 }
 export const DropdownToolItem: React.FC<Props> = ({ editor, depth, nodeName, attributeType, name, options }) => {
   const { state } = editor;
+  const { selection } = state;
   const node = getSelectedNode(state, depth);
   if(!node || !isNodeType(node, nodeName)) return null/*nothing to render - invalid node render*/;
 
   // == Handler ===================================================================
   const handleChange = (value: string) => {
-    editor.chain()
-          .updateAttributes(nodeName, { [attributeType]: value })
-          .setNodeSelection(state.selection.$anchor.pos)/*set the selection in the same position in case that the node was replaced*/
-          .run();
+    editor.commands.updateAttributes(nodeName, { [attributeType]: value });
+
+    const position = state.selection.$anchor.pos;
+    // set the selection in the same position in case that the node was replaced
+    if(isNodeSelection(selection)) editor.commands.setNodeSelection(position);
+    else editor.commands.setTextSelection(position);
 
     // Focus the editor again
     editor.commands.focus();
@@ -38,12 +39,8 @@ export const DropdownToolItem: React.FC<Props> = ({ editor, depth, nodeName, att
   // == UI ========================================================================
   const value = node.attrs[attributeType] ?? '' /*default*/;
   return (
-    <Flex width='full' alignItems='center' gap={2}>
-      <Box width={250} textAlign='right'>
-        <Text fontSize='14px'>{name}</Text>
-      </Box>
-      <DropdownTool name={name} onChange={handleChange} options={options} value={value}/>
-      <Box width={100}/>
-    </Flex>
+    <InputToolItemContainer name={name}>
+      <DropdownTool value={value} options={options} placeholder={name} onChange={handleChange}/>
+    </InputToolItemContainer>
   );
 };
