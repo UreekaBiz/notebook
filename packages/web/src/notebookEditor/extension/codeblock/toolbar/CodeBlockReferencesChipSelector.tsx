ca3b14620f18c10bb319a2c3b3968396e067ed64
ErickJoestar <erickborquez@gmail.com>
2022-08-23 16:17:32 -0500

    Add shortcut for code block references

--- packages/web/src/notebookEditor/extension/codeblock/toolbar/CodeBlockReferencesChipSelector.tsx
+++ packages/web/src/notebookEditor/extension/codeblock/toolbar/CodeBlockReferencesChipSelector.tsx
@@ -1,4 +1,6 @@
-import { getLogger, AttributeType, Logger, NodeName, REMOVED_CODEBLOCK_VISUALID } from '@ureeka-notebook/web-service';
+import { useEffect } from 'react';
+
+import { getLogger, getSelectedNode, isNodeSelection, AttributeType, Logger, NodeName, DATA_VISUAL_ID, REMOVED_CODEBLOCK_VISUALID } from '@ureeka-notebook/web-service';
 
 import { focusCodeBlock, visualIdFromCodeBlockReference } from 'notebookEditor/extension/codeblock/util';
 import { ChipValue } from 'notebookEditor/extension/shared/component/chipTool/Chip';
@@ -15,7 +17,51 @@ const log = getLogger(Logger.NOTEBOOK);
 interface Props extends EditorToolComponentProps {
   nodeName: NodeName;
 }
-export const CodeBlockReferencesChipSelector: React.FC<Props> = ({ editor, ...props }) => {
+export const CodeBlockReferencesChipSelector: React.FC<Props> = ({ editor, depth, nodeName, ...props }) => {
+  const { state } = editor,
+        { selection } = state;
+  const node = getSelectedNode(state, depth);
+
+  // == Effect ====================================================================
+  /** doing CMD + Click on a VisualId toggles it from the CodeBlockReferences */
+  useEffect(() => {
+    if(!node) return/*nothing to do*/;
+    const references = (node.attrs[AttributeType.CodeBlockReferences] ?? [] /*default*/) as string[];
+
+    const handler = async (event: MouseEvent) => {
+      if(!event.metaKey) return/*not a meta key is pressed -- nothing to do*/;
+
+      const { target } = event;
+      if(!target) return/*no target -- nothing to do*/;
+      if(!(target instanceof HTMLElement)) return/*not an element -- nothing to do*/;
+      const visualId = target.getAttribute(DATA_VISUAL_ID);
+      if(!visualId) return/*no visualId -- nothing to do*/;
+      const codeBlockReference = isValidCodeBlockReference(editor, visualId);
+      if(!codeBlockReference.isValid) return/*not a valid codeBlockReference -- nothing to do*/;
+      const codeblockId = codeBlockReference.codeBlockView.node.attrs[AttributeType.Id] ?? ''/*no id by default*/;
+
+      event.preventDefault();
+      event.stopPropagation();
+
+      // toggles the visualId from the CodeBlockReferences
+      const newValue = references.includes(codeblockId) ? references.filter(ref => ref !== codeblockId) : [...references, codeblockId];
+      editor.commands.updateAttributes(nodeName, { [AttributeType.CodeBlockReferences]: newValue });
+
+      // set the selection in the same position in case that the node was replaced
+      const position = state.selection.$anchor.pos;
+      if(isNodeSelection(selection)) editor.commands.setNodeSelection(position);
+      else editor.commands.setTextSelection(position);
+
+      // no need to focus editor again
+    };
+
+    window.addEventListener('mousedown', handler);
+
+    // removes the listener on unmount
+    return () => { window.removeEventListener('mousedown', handler); };
+  }, [editor, nodeName, node, selection, state.selection.$anchor.pos]);
+
+  // == Handler ===================================================================
   // SEE: CodeBlockReferenceChipSelector.tsx
   const getValueFromLabel = (visualId: string) => {
     const codeBlockReference = isValidCodeBlockReference(editor, visualId);
@@ -46,10 +92,13 @@ export const CodeBlockReferencesChipSelector: React.FC<Props> = ({ editor, ...pr
   // == UI ========================================================================
   return (
     <ChipToolItem
+      editor={editor}
+      nodeName={nodeName}
+      depth={depth}
       {...props}
+
       name='References'
       attributeType={AttributeType.CodeBlockReferences}
-      editor={editor}
       getLabelFromValue={getLabelFromValue}
       getValueFromLabel={getValueFromLabel}
       validate={validate}
