7cba72e73d1a577696b99f2d19947e118b3beb86
Alan Rodz <s061199@hotmail.com>
2022-08-23 22:15:24 -0500

    use equivalent

--- packages/service-common/src/notebookEditor/proseMirror/selection.ts
+++ packages/service-common/src/notebookEditor/proseMirror/selection.ts
@@ -104,23 +104,9 @@ const getNodeBefore = (selection: Selection) => {
 // == Range =======================================================================
 /**
  * computes the Range that holds all Nodes in between the start and end of the
- * Blocks located at the anchor and head of the given {@link Selection},
- * regardless of where the anchor and head are located in those Blocks
+ * Blocks located at the anchor and head of the given {@link Selection}
  */
-export const getBlockNodeRange = (selection: Selection) => {
-  const { pos: anchorPos } = selection.$anchor,
-        { pos: headPos } = selection.$head;
-
-  if(anchorPos < headPos) {
-    return {
-      from: anchorPos - selection.$anchor.parentOffset,
-      to: (headPos - selection.$head.parentOffset) + selection.$head.parent.nodeSize - 2/*account for the start and end of the parent Node*/,
-    };
-  } /* else -- head is past anchor */
-
-  // return the right range by inverting from and to
-  return {
-    from: headPos - selection.$head.parentOffset,
-    to: (anchorPos - selection.$anchor.parentOffset) + selection.$anchor.parent.nodeSize - 2/*account for the start and end of the parent Node*/,
-  };
-};
+ export const getBlockNodeRange = (selection: Selection) => ({
+  from: selection.from - selection.$from.parentOffset,
+  to: (selection.to - selection.$to.parentOffset) + selection.$to.parent.nodeSize - 2/*account for the start and end of the parent Node*/,
+});
