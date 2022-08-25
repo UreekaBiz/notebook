d92a7947f4d62435a9389a23508ff55744340fcb
Alan Rodz <s061199@hotmail.com>
2022-08-25 12:47:55 -0500

    missing change

--- packages/web/src/notebookEditor/extension/codeBlockReference/command.ts
+++ packages/web/src/notebookEditor/extension/codeBlockReference/command.ts
@@ -1,20 +1,8 @@
-
-import { createCodeBlockReferenceNode, generateNodeId, getSelectedNode, isCodeBlockReferenceNode, replaceAndSelectNode, AttributeType, Command, CommandFunctionType, NodeName } from '@ureeka-notebook/web-service';
+import { createCodeBlockReferenceNode, generateNodeId, getSelectedNode, isCodeBlockReferenceNode, replaceAndSelectNode, AttributeType, Command } from '@ureeka-notebook/web-service';
 
 import { focusChipToolInput } from 'notebookEditor/util';
 
-// == Type ========================================================================
-// NOTE: Usage of ambient module to ensure command is TypeScript-registered
-declare module '@tiptap/core' {
-  interface Commands<ReturnType> {
-    [NodeName.CODEBLOCK_REFERENCE/*Expected and guaranteed to be unique. (SEE: /notebookEditor/model/node)*/]: {
-      /** Insert and select a CodeBlockReference */
-      insertCodeBlockReference: CommandFunctionType<typeof insertAndSelectCodeBlockReferenceCommand, ReturnType>;
-    };
-  }
-}
-// == Implementation ==============================================================
-// .. Insert ......................................................................
+// ================================================================================
 export const insertAndSelectCodeBlockReferenceCommand: Command = (state, dispatch) => {
   const node = getSelectedNode(state);
   if(node && isCodeBlockReferenceNode(node)) return false/*ignore if selected node already is a CodeBlockReference*/;
