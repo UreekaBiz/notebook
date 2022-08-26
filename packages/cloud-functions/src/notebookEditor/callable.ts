07ed823357d4c4282138b066d31ef6fd53a096c7
Rob Grzywinski <rob.grzywinski@ureeka.biz>
2022-08-26 08:52:11 -0500

    Remove server-side execution examples

--- packages/cloud-functions/src/notebookEditor/callable.ts
+++ packages/cloud-functions/src/notebookEditor/callable.ts
@@ -1,39 +1,12 @@
 import * as functions from 'firebase-functions';
 
-import { NotebookEditorDemo2AsyncNodeExecute_Rest, NotebookEditorDemo2AsyncNodeExecute_Rest_Schema, NotebookEditorDemoAsyncNodeExecute_Rest, NotebookEditorDemoAsyncNodeExecute_Rest_Schema, NotebookEditorInsertNumbers_Rest, NotebookEditorInsertNumbers_Rest_Schema, NotebookEditorInsertText_Rest, NotebookEditorInsertText_Rest_Schema } from '@ureeka-notebook/service-common';
+import { NotebookEditorDemo2AsyncNodeExecute_Rest, NotebookEditorDemo2AsyncNodeExecute_Rest_Schema, NotebookEditorDemoAsyncNodeExecute_Rest, NotebookEditorDemoAsyncNodeExecute_Rest_Schema } from '@ureeka-notebook/service-common';
 
 import { wrapCall, SmallMemory } from '../util/function';
-import { EditorApi } from './api/api';
-import { InsertText } from './api/text';
 import { executeDemo2AsyncNode } from './demo2AsyncNode';
 import { executeDemoAsyncNode } from './demoAsyncNode';
 
 // ********************************************************************************
-// == Example Document Update =====================================================
-// inserts multiple numbers at random positions in the Notebook
-export const notebookEditorInsertNumbers = functions.runWith(SmallMemory).https.onCall(wrapCall<NotebookEditorInsertNumbers_Rest>(
-{ name: 'notebookEditorInsertNumbers', schema: NotebookEditorInsertNumbers_Rest_Schema, requiresAuth: true },
-async (data, context, userId) => {
-  const editorApi = new EditorApi();
-  const { versionIndex, document } = await editorApi.getDocument(userId!/*auth'd*/, data.notebookId);
-  // 10 (arbitrary) characters at random positions in the document
-  const updates =
-    Array.from({ length: 10 }, (_, i) => {
-      const position = Math.floor(Math.random() * document.content.size) + 1/*start of valid content*/;
-      return new InsertText(String(i), position, position);
-    });
-
-  await editorApi.updateDocument(userId!/*auth'd*/, data.notebookId, updates, { versionIndex }/*constrain for example*/)/*throws on error*/;
-}));
-
-// inserts the given text at the start of the Notebook
-export const notebookEditorInsertText = functions.runWith(SmallMemory).https.onCall(wrapCall<NotebookEditorInsertText_Rest>(
-{ name: 'notebookEditorInsertText', schema: NotebookEditorInsertText_Rest_Schema, requiresAuth: true },
-async (data, context, userId) => {
-  const editorApi = new EditorApi();
-  await editorApi.updateDocument(userId!/*auth'd*/, data.notebookId, [ new InsertText(data.text) ])/*throws on error*/;
-}));
-
 // == Demo Async Node =============================================================
 export const notebookEditorDemoAsyncNodeExecute = functions.runWith(SmallMemory).https.onCall(wrapCall<NotebookEditorDemoAsyncNodeExecute_Rest>(
 { name: 'notebookEditorDemoAsyncNodeExecute', schema: NotebookEditorDemoAsyncNodeExecute_Rest_Schema, requiresAuth: true },
--- packages/web-service/src/notebookEditor/function.ts
+++ packages/web-service/src/notebookEditor/function.ts
@@ -1,17 +1,11 @@
 import { httpsCallable } from 'firebase/functions';
 
-import { NotebookEditorDemo2AsyncNodeExecute_Rest, NotebookEditorDemoAsyncNodeExecute_Rest, NotebookEditorInsertNumbers_Rest, NotebookEditorInsertText_Rest, NotebookIdentifier } from '@ureeka-notebook/service-common';
+import { NotebookEditorDemo2AsyncNodeExecute_Rest, NotebookEditorDemoAsyncNodeExecute_Rest } from '@ureeka-notebook/service-common';
 
 import { functions } from '../util/firebase';
 import { wrapHttpsCallable } from '../util/function';
 
 // ** Auth'd User *****************************************************************
 // == Notebook Editor =============================================================
-export const notebookEditorInsertNumbers = wrapHttpsCallable<NotebookEditorInsertNumbers_Rest, NotebookIdentifier>(httpsCallable(functions, 'notebookEditorInsertNumbers'));
-
-export const notebookEditorInsertText = wrapHttpsCallable<NotebookEditorInsertText_Rest, NotebookIdentifier>(httpsCallable(functions, 'notebookEditorInsertText'));
-
-// -- Execute ---------------------------------------------------------------------
-export const notebookEditorDemo2AsyncNodeExecute = wrapHttpsCallable<NotebookEditorDemo2AsyncNodeExecute_Rest>(httpsCallable(functions, 'notebookEditorDemo2AsyncNodeExecute'));
-
 export const notebookEditorDemoAsyncNodeExecute = wrapHttpsCallable<NotebookEditorDemoAsyncNodeExecute_Rest>(httpsCallable(functions, 'notebookEditorDemoAsyncNodeExecute'));
+export const notebookEditorDemo2AsyncNodeExecute = wrapHttpsCallable<NotebookEditorDemo2AsyncNodeExecute_Rest>(httpsCallable(functions, 'notebookEditorDemo2AsyncNodeExecute'));
