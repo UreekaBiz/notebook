6561c5f0b6d82c7fc1c1b2d2e48ea163a68d497a
Alan Rodz <s061199@hotmail.com>
2022-08-29 18:05:56 -0500

    modify setParagraphCommand

--- packages/web/src/notebookEditor/extension/paragraph/command.ts
+++ packages/web/src/notebookEditor/extension/paragraph/command.ts
@@ -1,26 +1,38 @@
-import { getBlockNodeRange, getParagraphNodeType, isMarkHolderNode, Command } from '@ureeka-notebook/web-service';
+import { EditorState, Transaction } from 'prosemirror-state';
+
+import { getBlockNodeRange, getParagraphNodeType, isMarkHolderNode, AbstractDocumentUpdate, Command } from '@ureeka-notebook/web-service';
 
 // ********************************************************************************
-// --------------------------------------------------------------------------------
-// NOTE: this Command has to take into account the following constraints:
-//       1. Paragraphs set through this Command should not inherit any marks
-//       2. Paragraphs set through this Command should not have MarkHolders,
-//          because of 1, but they should (as expected) keep the remaining content
 export const setParagraphCommand: Command = (state, dispatch) => {
-  const { tr } = state;
-  const { from, to } = getBlockNodeRange(tr.selection);
-
-  // NOTE: removing MarkHolders before changing NodeType to ensure final
-  //       Selection does not change
-  tr.doc.nodesBetween(from, to, (node, pos) => {
-    if(isMarkHolderNode(node)) {
-      tr.delete(pos, pos + node.nodeSize);
-    } /* else -- ignore */
-  });
-
-  tr.setBlockType(from, to, getParagraphNodeType(state.schema))
-    .removeMark(from, to, null/*remove all marks*/);
-
-  dispatch(tr);
-  return true/*Command executed*/;
+  const updatedTr = new SetParagraphDocumentUpdate().update(state, state.tr);
+  dispatch(updatedTr);
+  return true/*command executed*/;
 };
+// NOTE: this DocumentUpdate has to take into account the following constraints:
+//       1. Paragraphs set through this DocumentUpdate should not inherit any marks
+//       2. Paragraphs set through this DocumentUpdate should not have MarkHolders,
+//          because of 1, but they should (as expected) keep the remaining content
+export class SetParagraphDocumentUpdate implements AbstractDocumentUpdate {
+  public constructor() {/*nothing additional*/}
+
+  /**
+   * modify the given Transaction such that a Paragraph Node is set following
+   * the given constraints (SEE: NOTE above) and return it
+   */
+  public update(editorState: EditorState, tr: Transaction) {
+    const { from, to } = getBlockNodeRange(tr.selection);
+
+    // NOTE: removing MarkHolders before changing NodeType to ensure final
+    //       Selection does not change
+    tr.doc.nodesBetween(from, to, (node, pos) => {
+      if(isMarkHolderNode(node)) {
+        tr.delete(pos, pos + node.nodeSize);
+      } /* else -- ignore */
+    });
+
+    tr.setBlockType(from, to, getParagraphNodeType(editorState.schema))
+      .removeMark(from, to, null/*remove all marks*/);
+
+    return tr/*updated*/;
+  }
+}
