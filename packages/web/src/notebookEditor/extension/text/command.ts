17df80ca46410b9902e2dac3a2c2ea1e7e7c4ae3
Alan Rodz <s061199@hotmail.com>
2022-08-29 17:51:38 -0500

    modify insertTabCommand

--- packages/web/src/notebookEditor/extension/text/command.ts
+++ packages/web/src/notebookEditor/extension/text/command.ts
@@ -1,11 +1,22 @@
-import { Command } from '@ureeka-notebook/web-service';
+import { EditorState, Transaction } from 'prosemirror-state';
+
+import { AbstractDocumentUpdate, Command } from '@ureeka-notebook/web-service';
 
 // ********************************************************************************
 /** Inserts a Tab. (SEE: ExtensionPriority) for details on handling */
 export const insertTabCommand: Command = (state, dispatch) => {
-  const { tr } = state;
-  tr.insertText('\t');
-
-  dispatch(tr);
-  return true/*Command executed*/;
+  const updatedTr = new InsertTabDocumentUpdate().update(state, state.tr);
+  dispatch(updatedTr);
+  return true/*command executed*/;
 };
+export class InsertTabDocumentUpdate implements AbstractDocumentUpdate {
+  public constructor() {/*nothing additional*/}
+
+  /**
+   * modify the given Transaction such that a Tab is inserted and return it
+   */
+  public update(editorState: EditorState, tr: Transaction) {
+    tr.insertText('\t');
+    return tr;
+  }
+}
