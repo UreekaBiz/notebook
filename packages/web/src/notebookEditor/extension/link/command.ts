f18bf73bbfe4ff9feb22269aa5d057328cec1669
Alan Rodz <s061199@hotmail.com>
2022-08-29 14:42:44 -0500

    command consistency

--- packages/web/src/notebookEditor/extension/link/command.ts
+++ packages/web/src/notebookEditor/extension/link/command.ts
@@ -1,6 +1,6 @@
 import { EditorState, Transaction } from 'prosemirror-state';
 
-import { setMarkCommand, toggleMarkCommand, unsetMarkCommand, AbstractDocumentUpdate, Command, LinkAttributes, MarkName, SetMarkDocumentUpdate, ToggleMarkDocumentUpdate, UnsetMarkDocumentUpdate, PREVENT_LINK_META } from '@ureeka-notebook/web-service';
+import { AbstractDocumentUpdate, Command, LinkAttributes, MarkName, SetMarkDocumentUpdate, ToggleMarkDocumentUpdate, UnsetMarkDocumentUpdate, PREVENT_LINK_META } from '@ureeka-notebook/web-service';
 
 // ********************************************************************************
 // NOTE: the desired behavior for these Commands is that creating a Link in a
@@ -11,8 +11,9 @@ import { setMarkCommand, toggleMarkCommand, unsetMarkCommand, AbstractDocumentUp
 // == Implementation ==============================================================
 /** set the Link Mark across the current Selection */
 export const setLinkCommand = (attributes: Partial<LinkAttributes>): Command => (state, dispatch) => {
-  state.tr.setMeta(PREVENT_LINK_META, true/*(SEE: ../plugin.ts)*/);
-  return setMarkCommand(MarkName.LINK, attributes)(state, dispatch);
+  const updatedTr = new SetLinkDocumentUpdate(attributes).update(state, state.tr);
+  dispatch(updatedTr);
+  return true/*command executed*/;
 };
 export class SetLinkDocumentUpdate implements AbstractDocumentUpdate {
   public constructor(private readonly attributes: Partial<LinkAttributes>) {/*nothing additional*/}
@@ -31,8 +32,9 @@ export class SetLinkDocumentUpdate implements AbstractDocumentUpdate {
 // --------------------------------------------------------------------------------
 /** unset the Link Mark across the current Selection */
 export const unsetLinkCommand = (): Command => (state, dispatch) => {
-  state.tr.setMeta(PREVENT_LINK_META, true/*(SEE: ../plugin.ts)*/);
-  return unsetMarkCommand(MarkName.LINK, true/*extend empty Mark Range*/)(state, dispatch);
+  const updatedTr = new UnsetLinkDocumentUpdate().update(state, state.tr);
+  dispatch(updatedTr);
+  return true/*command executed*/;
 };
 export class UnsetLinkDocumentUpdate implements AbstractDocumentUpdate {
   public constructor() {/*nothing additional*/}
@@ -54,8 +56,9 @@ export class UnsetLinkDocumentUpdate implements AbstractDocumentUpdate {
  * whether or not it is currently active
  */
 export const toggleLinkCommand = (attributes: Partial<LinkAttributes>): Command => (state, dispatch) => {
-  state.tr.setMeta(PREVENT_LINK_META, true/*(SEE: ../plugin.ts)*/);
-  return toggleMarkCommand(MarkName.LINK, attributes)(state, dispatch);
+  const updatedTr = new ToggleLinkDocumentUpdate(attributes).update(state, state.tr);
+  dispatch(updatedTr);
+  return true/*command executed*/;
 };
 export class ToggleLinkDocumentUpdate implements AbstractDocumentUpdate {
   public constructor(private readonly attributes: Partial<LinkAttributes>) {/*nothing additional*/}
