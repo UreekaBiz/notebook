fe55e0043ccd86227339336a4c076f331108afcb
Alan Rodz <s061199@hotmail.com>
2022-08-29 17:53:20 -0500

    modify toggleSuperScriptCommand

--- packages/web/src/notebookEditor/extension/superScript/command.ts
+++ packages/web/src/notebookEditor/extension/superScript/command.ts
@@ -1,9 +1,25 @@
-import { Command, MarkName } from '@ureeka-notebook/web-service';
+import { EditorState, Transaction } from 'prosemirror-state';
 
-import { toggleOrSetMarkCommand } from '../markHolder/command';
+import { AbstractDocumentUpdate, Command, MarkName } from '@ureeka-notebook/web-service';
+
+import { ToggleOrSetMarkDocumentUpdate } from '../markHolder/command';
 
 // ********************************************************************************
-export const toggleSuperScriptCommand: Command = (state, dispatch) =>
-  toggleOrSetMarkCommand(MarkName.SUPER_SCRIPT, state.schema.marks[MarkName.SUPER_SCRIPT])(state, dispatch);
+/** toggle the Superscript Mark */
+export const toggleSuperScriptCommand: Command = (state, dispatch) => {
+  const updatedTr = new ToggleSuperScriptDocumentUpdate().update(state, state.tr);
+  dispatch(updatedTr);
+  return true/*command executed*/;
+};
+export class ToggleSuperScriptDocumentUpdate implements AbstractDocumentUpdate {
+  public constructor() {/*nothing additional*/}
 
-
+  /**
+   * modify the given Transaction such that the Superscript Mark
+   * is toggled and return it
+   */
+  public update(editorState: EditorState, tr: Transaction) {
+    const updatedTr = new ToggleOrSetMarkDocumentUpdate(MarkName.SUPER_SCRIPT, editorState.schema.marks[MarkName.SUPER_SCRIPT]).update(editorState, tr);
+    return updatedTr;
+  }
+}
