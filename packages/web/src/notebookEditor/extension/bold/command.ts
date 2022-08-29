0b51a55dc878313fae17b26c972f3d3376b2db21
Alan Rodz <s061199@hotmail.com>
2022-08-29 17:42:22 -0500

    modify toggleBoldCommand

--- packages/web/src/notebookEditor/extension/bold/command.ts
+++ packages/web/src/notebookEditor/extension/bold/command.ts
@@ -1,8 +1,25 @@
-import { Command, MarkName } from '@ureeka-notebook/web-service';
+import { EditorState, Transaction } from 'prosemirror-state';
 
-import { toggleOrSetMarkCommand } from '../markHolder/command';
+import { AbstractDocumentUpdate, Command, MarkName } from '@ureeka-notebook/web-service';
+
+import { ToggleOrSetMarkDocumentUpdate } from '../markHolder/command';
 
 // ********************************************************************************
-export const toggleBoldCommand: Command = (state, dispatch) =>
-  toggleOrSetMarkCommand(MarkName.BOLD, state.schema.marks[MarkName.BOLD])(state, dispatch);
+/** toggle the Bold Mark */
+export const toggleBoldCommand: Command = (state, dispatch) => {
+  const updatedTr = new ToggleBoldDocumentUpdate().update(state, state.tr);
+  dispatch(updatedTr);
+  return true/*command executed*/;
+};
+export class ToggleBoldDocumentUpdate implements AbstractDocumentUpdate {
+  public constructor() {/*nothing additional*/}
 
+  /**
+   * modify the given Transaction such that the Bold Mark
+   * is toggled and return it
+   */
+  public update(editorState: EditorState, tr: Transaction) {
+    const updatedTr = new ToggleOrSetMarkDocumentUpdate(MarkName.BOLD, editorState.schema.marks[MarkName.BOLD]).update(editorState, tr);
+    return updatedTr;
+  }
+}
