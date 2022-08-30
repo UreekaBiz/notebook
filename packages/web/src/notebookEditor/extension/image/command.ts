775de4b39d6d200c37b22c1be6e10172eca1fae7
Alan Rodz <s061199@hotmail.com>
2022-08-30 10:32:10 -0500

    match pm-devtool (match pm return semantics for image branch)

--- packages/web/src/notebookEditor/extension/image/command.ts
+++ packages/web/src/notebookEditor/extension/image/command.ts
@@ -1,15 +1,19 @@
 import { Editor } from '@tiptap/core';
 import { EditorState, Transaction } from 'prosemirror-state';
 
-import { createImageNode, isNodeSelection, AbstractDocumentUpdate, AttributeType, Command, ImageAttributes, ReplaceAndSelectNodeDocumentUpdate, VerticalAlign } from '@ureeka-notebook/web-service';
+import { createImageNode, isNodeSelection, AbstractDocumentUpdate, AttributeType, Command, ImageAttributes, NotebookSchemaType, ReplaceAndSelectNodeDocumentUpdate, VerticalAlign } from '@ureeka-notebook/web-service';
 
 // ================================================================================
 // creates and selects an Image Node by replacing whatever is at the current
 // selection with the newly created Image Node
 export const insertAndSelectImageCommand = (attributes: Partial<ImageAttributes>): Command => (state, dispatch) => {
   const updatedTr = new InsertAndSelectImageDocumentUpdate(attributes).update(state, state.tr);
-  dispatch(updatedTr);
-  return true/*command executed*/;
+  if(updatedTr) {
+    dispatch(updatedTr);
+    return true/*Command executed*/;
+  } /* else -- Command cannot be executed */
+
+  return false/*not executed*/;
 };
 export class InsertAndSelectImageDocumentUpdate implements AbstractDocumentUpdate {
   public constructor(private readonly attributes: Partial<ImageAttributes>) {/*nothing additional*/}
@@ -18,10 +22,10 @@ export class InsertAndSelectImageDocumentUpdate implements AbstractDocumentUpdat
    * modify the given Transaction such that an Image Node is created and
    * replaces the current Selection, then return it
    */
-  public update(editorState: EditorState, tr: Transaction) {
+  public update(editorState: EditorState<NotebookSchemaType>, tr: Transaction<NotebookSchemaType>) {
     const image = createImageNode(editorState.schema, this.attributes);
     const updatedTr =  new ReplaceAndSelectNodeDocumentUpdate(image).update(editorState, editorState.tr);
-    return updatedTr;
+    return updatedTr/*updated*/;
   }
 }
 
