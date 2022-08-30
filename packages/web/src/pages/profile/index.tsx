fd15ca9b336e15e63d2ba55641b5117d0705b5bb
ErickJoestar <erickborquez@gmail.com>
2022-08-30 14:33:55 -0500

    add basic profile pages

---
+++ packages/web/src/pages/profile/index.tsx
@@ -0,0 +1,26 @@
+import { Box, Flex, Heading } from '@chakra-ui/react';
+
+import { RequiredAuthUserWrapper } from 'authUser/RequiredAuthUserWrapper';
+import { WrappedPage } from 'core/wrapper';
+import { NotebookServiceWrapper } from 'notebook/NotebookServiceWrapper';
+import { ProfileNavigationLayout } from 'shared/layout/ProfileNavigationLayout';
+
+// ********************************************************************************
+// NOTE: this page is using Static Site Generation. See pages/README.md
+// == Client Side =================================================================
+function APIKeyPage() {
+  return (
+    <Box>
+      <Flex alignItems='center' justifyContent='space-between' width='full' marginBottom={4}>
+        <Heading flex='1 1'>Home</Heading>
+      </Flex>
+    </Box>
+  );
+}
+
+// --------------------------------------------------------------------------------
+// SEE: core/wrapper.tsx for more information
+const Page: WrappedPage = APIKeyPage;
+      Page.wrappers = [RequiredAuthUserWrapper, NotebookServiceWrapper, ProfileNavigationLayout]/*outer to inner order*/;
+
+export default Page;
