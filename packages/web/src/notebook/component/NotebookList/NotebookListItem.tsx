b624ea96fe9c3caa15161b97e97a17f1d1f096b9
ErickJoestar <erickborquez@gmail.com>
2022-08-31 11:42:35 -0500

    add share counts to NotebookListItem

--- packages/web/src/notebook/component/NotebookList/NotebookListItem.tsx
+++ packages/web/src/notebook/component/NotebookList/NotebookListItem.tsx
@@ -1,9 +1,12 @@
 import { Box, Flex, IconButton, Link, Menu, MenuButton, MenuItem, MenuList, Text } from '@chakra-ui/react';
 import NextLink from 'next/link';
+import { BiPencil } from 'react-icons/bi';
 import { BsThreeDots } from 'react-icons/bs';
 import { HiTrash } from 'react-icons/hi';
+import { MdOutlineRemoveRedEye } from 'react-icons/md';
 
-import { NotebookTuple } from '@ureeka-notebook/web-service';
+import { getNotebookShareCounts, NotebookTuple } from '@ureeka-notebook/web-service';
+
 import { notebookRoute } from 'shared/routes';
 import { getMinifiedReadableDate } from 'ui/util';
 
@@ -14,11 +17,13 @@ interface Props {
 export const NotebookListItem: React.FC<Props> = ({ notebookTuple }) => {
   const { id, obj } = notebookTuple;
 
+  const { editors, viewers } = getNotebookShareCounts(obj);
+
   return (
     <Flex alignItems='center'>
-      <Box flex='1 1' whiteSpace='nowrap' overflow='hidden' paddingRight={4}>
+      <Box flex='1 1' whiteSpace='nowrap' overflow='hidden' paddingRight={2}>
         <NextLink href={notebookRoute(id)} passHref>
-          <Link display='block' color='#222' fontSize='15px' fontWeight={600} textOverflow='ellipsis' overflow='hidden'>
+          <Link display='block' flex='1 1' color='#444' fontSize='15px' fontWeight={600} textOverflow='ellipsis' overflow='hidden'>
             {obj.name}
           </Link>
         </NextLink>
@@ -27,6 +32,18 @@ export const NotebookListItem: React.FC<Props> = ({ notebookTuple }) => {
           <Text marginLeft={1} color='#999' fontWeight={600}>{getMinifiedReadableDate(obj.updateTimestamp.toDate())}</Text>
         </Flex>
       </Box>
+
+      <Box width='34px' color='#BBB' fontSize={12} fontWeight={600}>
+        <Flex alignItems='center' >
+          <BiPencil/>
+          <Text marginLeft={1}>{editors}</Text>
+        </Flex>
+        <Flex alignItems='center'>
+          <MdOutlineRemoveRedEye />
+          <Text marginLeft={1}>{viewers}</Text>
+        </Flex>
+      </Box>
+
       <Menu>
         <MenuButton
           as={IconButton}
