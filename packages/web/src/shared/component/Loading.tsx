159d08d5aedfe5e66eb1a18e0f76b374b87e8a81
ErickJoestar <erickborquez@gmail.com>
2022-09-02 14:11:13 -0500

    documentation

--- packages/web/src/shared/component/Loading.tsx
+++ packages/web/src/shared/component/Loading.tsx
@@ -1,10 +1,12 @@
 import { Flex, Spinner } from '@chakra-ui/react';
 
 // ********************************************************************************
-/** displays a loading indicator after 300ms */
+/** displays a loading indicator after 1s */
+// NOTE: this is useful when loading content async that requires a loading
+//       indicator but the loading time is so short that it is not worth displaying
+//       the loading indicator instantly.
 export const Loading = () =>  (
   <Flex
-
     className='lazy-shown' // SEE: index.css
     alignItems='center'
     justifyContent='center'
