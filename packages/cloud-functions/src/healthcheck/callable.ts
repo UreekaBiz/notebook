93c225f1cc3033dc8e2d323a50c273d46bcf7e4f
Rob Grzywinski <rob.grzywinski@ureeka.biz>
2022-09-08 10:16:44 -0500

    Consistency

--- packages/cloud-functions/src/healthcheck/callable.ts
+++ packages/cloud-functions/src/healthcheck/callable.ts
@@ -2,11 +2,11 @@ import * as functions from 'firebase-functions';
 
 import { HealthcheckResult } from '@ureeka-notebook/service-common';
 
-import { wrapRequest, LongrunningRuntimeOpts } from '../util/function';
+import { wrapRequest, LongRunningRuntimeOpts } from '../util/function';
 import { doHealthcheck } from './healthcheck';
 
 // ********************************************************************************
-export const healthcheck = functions.runWith(LongrunningRuntimeOpts).https.onRequest(wrapRequest<object/*none*/, HealthcheckResult>(
+export const healthcheck = functions.runWith(LongRunningRuntimeOpts).https.onRequest(wrapRequest<object/*none*/, HealthcheckResult>(
 { name: 'healthcheck', requiresAuth: false/*public*/ },
 async () => {
   return await doHealthcheck();
