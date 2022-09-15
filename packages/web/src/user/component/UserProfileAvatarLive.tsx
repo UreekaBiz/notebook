ad0cde5d6ef079947e780247354b8287f2a0c29e
ErickJoestar <erickborquez@gmail.com>
2022-09-15 15:31:55 -0500

    Create UserProfileAvatarLive

---
+++ packages/web/src/user/component/UserProfileAvatarLive.tsx
@@ -0,0 +1,22 @@
+import { AvatarProps } from '@chakra-ui/react';
+
+import { UserIdentifier } from '@ureeka-notebook/web-service';
+import { useUserProfile } from 'user/hook/useUserProfile';
+
+import { UserProfileAvatar } from './UserProfileAvatar';
+
+// live version of the UserProfileAvatar component
+// ********************************************************************************
+type Props = AvatarProps & {
+  userId: UserIdentifier;
+  // NOTE: Overriding BoxProps to require a defined value
+  width: number | string;
+  height: number | string;
+};
+export const UserProfileAvatarLive: React.FC<Props> = ({ userId, ...props }) => {
+  const { userProfile } = useUserProfile(userId);
+
+  if(!userProfile) return null;
+
+  return <UserProfileAvatar userId={userId} userPublicProfile={userProfile} {...props} />;
+};
