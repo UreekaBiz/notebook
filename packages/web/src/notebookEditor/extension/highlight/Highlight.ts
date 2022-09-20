25e94082cc0feec51de978f9041cf7a8a03d0a07
Alan Rodz <s061199@hotmail.com>
2022-09-20 12:34:21 -0500

    match pm-devtool (modify highlight plugin)

--- packages/web/src/notebookEditor/extension/highlight/Highlight.ts
+++ packages/web/src/notebookEditor/extension/highlight/Highlight.ts
@@ -1,5 +1,5 @@
-import { Extension } from '@tiptap/core';
-import { Plugin, EditorState } from 'prosemirror-state';
+import { isNodeSelection, Editor, Extension } from '@tiptap/core';
+import { EditorState, Plugin } from 'prosemirror-state';
 import { Decoration, DecorationSet } from 'prosemirror-view';
 
 import { ExtensionName } from 'notebookEditor/model/type';
@@ -14,20 +14,36 @@ export const Highlight = Extension.create({
   name: ExtensionName.HIGHLIGHT/*Expected and guaranteed to be unique*/,
 
   // -- Plugin --------------------------------------------------------------------
-  addProseMirrorPlugins() {
-    return [
-      new Plugin({
-        props: {
-          decorations(state: EditorState) {
-            const selection = state.selection;
-            const from = selection.from,
-                  to = selection.to;
+  addProseMirrorPlugins() {return [highlightPlugin(this.editor)];},
+});
 
-            const decoration = Decoration.inline(from, to, { class: SELECTED_TEXT_CLASS });
-            return DecorationSet.create(state.doc, [decoration]);
-          },
-        },
-      }),
-    ];
+const highlightPlugin = (editor: Editor) => new Plugin({
+  props: {
+    decorations(state: EditorState) {
+      if(editor.view.hasFocus()) return undefined/*only show decoration if View is not focused*/;
+
+      const { selection } = state;
+      if(isNodeSelection(selection)) return undefined/*do nothing when a Node is selected*/;
+
+      const { empty, from, to } = selection;
+      if(empty) return undefined/*nothing to select*/;
+
+      const decorations: Decoration[] = [];
+      state.doc.nodesBetween(from, to, (node, pos, parent) => {
+        if(node.isText && parent.isTextblock/*parent is not a nested Editor View*/) {
+          const nodeStart = pos;
+          const nodeEnd = pos + node.nodeSize;
+
+          if(nodeStart <= from) {
+            decorations.push(Decoration.inline(from, from + (Math.min(nodeEnd, to) - from), { class: SELECTED_TEXT_CLASS }));
+          } else {
+            // check must be done backwards
+            decorations.push(Decoration.inline(nodeStart, nodeStart + (to - nodeStart), { class: SELECTED_TEXT_CLASS }));
+          }
+        } /* else -- ignore Node */
+      });
+
+      return DecorationSet.create(state.doc, decorations);
+    },
   },
 });
