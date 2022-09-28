5e4311f2adbad78688a0898602f3a0820286b55b
Alan Rodz <s061199@hotmail.com>
2022-09-28 09:55:01 -0500

    bring constant

--- packages/web/src/notebookEditor/theme/theme.ts
+++ packages/web/src/notebookEditor/theme/theme.ts
@@ -12,6 +12,8 @@ export const ACTIVE_BUTTON_COLOR = '#E2E8F0';
 export const ICON_BUTTON_CLASS = 'iconButton';
 
 // .. Node ........................................................................
+// added to the selected Node by default by ProseMirror
+export const PM_SELECTED_CLASS = 'ProseMirror-selectednode';
 export const INLINE_NODE_CONTAINER_CLASS = 'inlineNodeContainer';
 export const SELECTED_TEXT_CLASS = 'selected_text';
 
