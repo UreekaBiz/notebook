3a26a9112604e96a720d2e242c8dc8d9f7de82dc
ErickJoestar <erickborquez@gmail.com>
2022-09-30 10:06:07 -0500

    Panel tabs

--- packages/web/src/notebookEditor/sidebar/component/SidebarPanelsNavigation/SidebarPanelsNavigation.tsx
+++ packages/web/src/notebookEditor/sidebar/component/SidebarPanelsNavigation/SidebarPanelsNavigation.tsx
@@ -1,8 +1,32 @@
-import { Flex } from '@chakra-ui/react';
+import { Flex, Text } from '@chakra-ui/react';
+import { ReactElement } from 'react';
+import { AiOutlineEdit } from 'react-icons/ai';
+import { CgList } from 'react-icons/cg';
 
-import { SidebarPanel } from 'notebookEditor/sidebar/type';
+import { getReadableSidebarPanel, SidebarPanel } from 'notebookEditor/sidebar/type';
 
-import { SidebarPanelsNavigationItem } from './SidebarPanelsNavigationItem';
+// ********************************************************************************
+type Panel = {
+  panel: SidebarPanel;
+
+  label: string;
+  icon: ReactElement;
+};
+
+const panels: Panel[] = [
+  {
+    panel: SidebarPanel.Toolbar,
+
+    label: getReadableSidebarPanel(SidebarPanel.Toolbar),
+    icon: <AiOutlineEdit/>,
+  },
+  {
+    panel: SidebarPanel.Outline,
+
+    label: getReadableSidebarPanel(SidebarPanel.Outline),
+    icon: <CgList/>,
+  },
+];
 
 // ********************************************************************************
 interface Props {
@@ -11,15 +35,37 @@ interface Props {
 }
 export const SidebarPanelsNavigation: React.FC<Props> = ({ value, onChange }) => {
   return (
-    <Flex alignItems='flex-end'>
-      {Object.values(SidebarPanel).map((panel) => (
-        <SidebarPanelsNavigationItem
-          key={panel}
-          panel={panel}
-          isActive={value === panel}
-          onClick={() => onChange(panel)}
-        />
-      ))}
+    <Flex
+      justifyContent='space-between'
+      gap='8px'
+      paddingX='8px'
+      boxShadow='inner'
+    >
+      {panels.map(({ panel, icon, label }) => {
+        const isActive = panel === value;
+        return (
+          <Flex
+            key={panel}
+            direction='column'
+            alignItems='center'
+            justifyContent='center'
+            flex='1 1'
+            height='60px'
+            color={isActive ? '#1967D2' : '#666'}
+            fontSize='18px'
+            _hover={{
+              cursor: 'pointer',
+              color: isActive ? '#1967D2' : '#333',
+            }}
+            onClick={() => onChange(panel)}
+          >
+            {icon}
+            <Text fontSize='12px' fontWeight='700' >
+              {label}
+            </Text>
+          </Flex>
+        );
+      })}
     </Flex>
   );
 };
--- packages/web/src/notebookEditor/sidebar/component/SidebarPanelsNavigation/SidebarPanelsNavigationItem.tsx
+++ packages/web/src/notebookEditor/sidebar/component/SidebarPanelsNavigation/SidebarPanelsNavigationItem.tsx
@@ -1,32 +1,29 @@
-import { Box, BoxProps, Text } from '@chakra-ui/react';
+import { Box, BoxProps } from '@chakra-ui/react';
+import { ReactElement } from 'react';
 
-import { getReadableSidebarPanel, SidebarPanel } from 'notebookEditor/sidebar/type';
+import { SidebarPanel } from 'notebookEditor/sidebar/type';
 
 // ********************************************************************************
 interface Props extends BoxProps {
   panel: SidebarPanel;
 
+  icon: ReactElement;
+
   isActive?: boolean;
 }
-export const SidebarPanelsNavigationItem: React.FC<Props> = ({ panel, isActive, ...props }) => {
+export const SidebarPanelsNavigationItem: React.FC<Props> = ({ panel, icon, isActive, ...props }) => {
   return (
     <Box
       marginX='2px'
       padding='2px 8px'
-      paddingBottom={isActive ? '6px' : undefined}
-      background={isActive ? '#EAEAEA' : '#F3F3F3'}
-      borderRadius='8px 8px 0 0'
-      border='1px solid'
-      borderColor='gray.300'
-      fontSize='12px'
-      fontWeight='500'
       transition='all 0.2s'
+      color={isActive ? '#EEE': '#AAA'}
       _hover={{
         cursor: 'pointer',
       }}
       {...props}
     >
-      <Text color='#333'>{getReadableSidebarPanel(panel)}</Text>
+      {icon}
     </Box>
   );
 };
