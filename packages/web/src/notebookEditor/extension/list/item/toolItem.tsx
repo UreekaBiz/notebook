564bab365f05f0f7fb43363578b601f698e5a8a5
Alan Rodz <s061199@hotmail.com>
2022-10-10 22:55:18 -0500

    add checks

--- packages/web/src/notebookEditor/extension/list/item/toolItem.tsx
+++ packages/web/src/notebookEditor/extension/list/item/toolItem.tsx
@@ -1,12 +1,13 @@
 import { MdFormatIndentDecrease, MdFormatIndentIncrease } from 'react-icons/md';
 
-import { isNodeSelection } from '@ureeka-notebook/web-service';
+import { getParentNode, isListItemContentNode, isNodeSelection } from '@ureeka-notebook/web-service';
 
 import { toolItemCommandWrapper } from 'notebookEditor/command/util';
 import { ToolItem } from 'notebookEditor/sidebar/toolbar/type';
 
 import { dedentListCommand } from '../keyboardShortcut/dedent';
 import { indentListCommand } from '../keyboardShortcut/indent';
+import { shouldShowToolItemInsideList } from '../util';
 
 // ********************************************************************************
 // -- Indentation -----------------------------------------------------------------
@@ -20,6 +21,13 @@ export const dedentListToolItem: ToolItem = {
   tooltip: 'Dedent List (⇧ + ↹)',
 
   shouldBeDisabled: (editor) => isNodeSelection(editor.state.selection),
+  shouldShow: (editor, depth) => {
+    if(isListItemContentNode(getParentNode(editor.state.selection))) {
+      return shouldShowToolItemInsideList(editor.state, depth);
+    } /* else -- not inside ListItemContent */
+
+    return depth === undefined || editor.state.selection.$anchor.depth === depth;/*direct parent*/
+  },
   onClick: (editor, depth) => toolItemCommandWrapper(editor, depth, dedentListCommand),
 };
 
@@ -33,6 +41,13 @@ export const indentListToolItem: ToolItem = {
   tooltip: 'Indent List (↹)',
 
   shouldBeDisabled: (editor) => isNodeSelection(editor.state.selection),
+  shouldShow: (editor, depth) => {
+    if(isListItemContentNode(getParentNode(editor.state.selection))) {
+      return shouldShowToolItemInsideList(editor.state, depth);
+    } /* else -- not inside ListItemContent */
+
+    return depth === undefined || editor.state.selection.$anchor.depth === depth;/*direct parent*/
+  },
   onClick: (editor, depth) => toolItemCommandWrapper(editor, depth, indentListCommand),
 };
 
