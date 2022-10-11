96e0a0e76daf9401965c789de3751062842af839
Alan Rodz <s061199@hotmail.com>
2022-10-11 13:29:28 -0500

    prevent block leave

--- packages/web/src/notebookEditor/extension/keymap/Keymap.ts
+++ packages/web/src/notebookEditor/extension/keymap/Keymap.ts
@@ -1,4 +1,7 @@
-import { Extension } from '@tiptap/core';
+import { Editor, Extension } from '@tiptap/core';
+import { Node as ProseMirrorNode } from 'prosemirror-model';
+
+import { isInlineNodeWithContent, setNodeSelectionCommand } from '@ureeka-notebook/web-service';
 
 import { shortcutCommandWrapper } from 'notebookEditor/command/util';
 import { ExtensionName, ExtensionPriority, NoOptions, NoStorage } from 'notebookEditor/model/type';
@@ -27,6 +30,38 @@ export const Keymap = Extension.create<NoOptions, NoStorage>({
 
       // indent at Block level
       'Mod-]': () => shortcutCommandWrapper(this.editor, changeBlockIndentationCommand('indent')),
+
+      // prevent incorrect Selection when there is an InlineNodeWithContent whose
+      // size would cause the Cursor to jump to either the top or the bottom
+      // of the Document
+      'ArrowUp': () => selectInlineNodeWithContent(this.editor, 'up'),
+      'ArrowDown': () => selectInlineNodeWithContent(this.editor, 'down'),
     };
   },
 });
+
+// == Util ========================================================================
+// prevent the Cursor from jumping to the very top or end of the Document whenever
+// the size of an InlineNodeWithContent would make it do so, and instead, select it
+const selectInlineNodeWithContent = (editor: Editor,  arrowDir: 'up' | 'down') => {
+  const { empty, $from, from } = editor.state.selection;
+  if(!empty) return false/*let PM handle the event*/;
+
+  let nodeNearby: ProseMirrorNode | null/*not set*/;
+  let nodeNearbyPos: number;
+  try {
+    if(arrowDir === 'up') { nodeNearby = $from.nodeBefore; }
+    else { nodeNearby = $from.nodeAfter; }
+
+    if(arrowDir === 'up') { nodeNearbyPos = Math.max(from-1/*nodeBefore*/, 0/*do not go behind Doc*/); }
+    else { nodeNearbyPos = Math.min(from/*position can be a TextSelection and a NodeSelection at the same time*/, editor.state.doc.nodeSize/*do not go past Doc*/); }
+
+    if(nodeNearby && isInlineNodeWithContent(nodeNearby)) {
+      return shortcutCommandWrapper(editor, setNodeSelectionCommand(nodeNearbyPos));
+    } else {
+      return false/*let PM handle the event*/;
+    }
+  } catch(error) {
+    return false/*invalid resulting Selection*/;
+  }
+};
