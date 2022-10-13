bd87b7f47af0e06b8a65b85f76aaef48afca59ff
Alan Rodz <s061199@hotmail.com>
2022-10-13 11:54:23 -0500

    match pm-devtool (add basic scrolling to preview)

--- packages/web/src/notebookEditor/component/NotebookViewer.tsx
+++ packages/web/src/notebookEditor/component/NotebookViewer.tsx
@@ -3,15 +3,21 @@ import { convertContentToHTML, NotebookDocumentContent } from '@ureeka-notebook/
 import { EDITOR_CLASS_NAME, EDITOR_PREVIEW_CLASS_NAME } from 'core/theme';
 
 // ********************************************************************************
+// == Constant ====================================================================
+export const NOTEBOOK_VIEWER_ID = 'notebookViewerId';
+
+// == Interface ====================================================================
 interface Props {
   content: NotebookDocumentContent;
 }
+
+// == Component ===================================================================
 export const NotebookViewer: React.FC<Props>= ({ content }) => {
   // uses `dangerouslySetInnerHTML` to the corresponding HTML string of the Notebook.
   // This is a security risk, but it is the only way to render the content of a
   // Notebook at the moment.
   const htmlContent = convertContentToHTML(content);
   return (
-    <div className={`${EDITOR_CLASS_NAME} ${EDITOR_PREVIEW_CLASS_NAME}`}/*SEE: /index.css*/ dangerouslySetInnerHTML={{ __html: htmlContent ?? '' }} />
+    <div id={NOTEBOOK_VIEWER_ID} className={`${EDITOR_CLASS_NAME} ${EDITOR_PREVIEW_CLASS_NAME}`}/*SEE: /index.css*/ dangerouslySetInnerHTML={{ __html: htmlContent ?? '' }} />
   );
 };
