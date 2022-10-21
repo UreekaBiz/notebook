4fb8c4d202a68f4447fc0ea19c15cc30991e5226
Alan Rodz <s061199@hotmail.com>
2022-10-21 12:32:02 -0500

    match pm-devtool (make renderNodeContent abstract)

--- packages/web/src/notebookEditor/extension/nestedViewNode/nestedViewBlockNode/nodeView/view.ts
+++ packages/web/src/notebookEditor/extension/nestedViewNode/nestedViewBlockNode/nodeView/view.ts
@@ -1,6 +1,6 @@
 import { Editor } from '@tiptap/core';
 
-import { getPosType, NestedViewBlockNodeType } from '@ureeka-notebook/web-service';
+import { getNestedViewNodeTextString, getPosType, NestedViewBlockNodeType, NESTED_VIEW_NODE_EMPTY_NODE_CLASS } from '@ureeka-notebook/web-service';
 
 import { AbstractNestedViewNodeView } from 'notebookEditor/extension/nestedViewNode/nodeView/view';
 
@@ -14,5 +14,31 @@ export class NestedViewBlockNodeView extends AbstractNestedViewNodeView<NestedVi
     super(model, editor, node, storage, getPos);
 
     // currently nothing else required
+  }
+
+  // NOTE: matches the renderNodeContent behavior of the EditableInlineNodeWithContent
+  // display the contents of the inner View as a Block showing their length
+  public renderNodeContent(): void {
+    if(!this.renderDisplayContainer) return/*not set yet, nothing to do*/;
+
+		// get text string to render
+    const textString = getNestedViewNodeTextString(this.node);
+		if(textString.length < 1) {
+			this.dom.classList.add(NESTED_VIEW_NODE_EMPTY_NODE_CLASS);
+
+			// clear rendered DOM Nodes, since this Node is in an invalid state
+			while(this.renderDisplayContainer.firstChild){
+        this.renderDisplayContainer.firstChild.remove();
+      }
+      return/*nothing left to do*/;
+		} /* else -- not empty */
+
+    this.dom.classList.remove(NESTED_VIEW_NODE_EMPTY_NODE_CLASS);
+
+    // show the default representation of the content of this Node
+    this.renderDisplayContainer.firstChild?.remove();
+    const newRenderDisplayChild = document.createElement('span');
+          newRenderDisplayChild.innerHTML = `Length: ${textString.length}`;
+    this.renderDisplayContainer.appendChild(newRenderDisplayChild);
   }
 }
--- packages/web/src/notebookEditor/extension/nestedViewNode/nodeView/view.ts
+++ packages/web/src/notebookEditor/extension/nestedViewNode/nodeView/view.ts
@@ -1,7 +1,7 @@
 import { Editor } from '@tiptap/core';
 import { EditorView } from 'prosemirror-view';
 
-import { getNestedViewNodeTextString, getPosType, isEditableInlineNodeWithContentNode, NestedViewNodeType, DATA_NODE_TYPE, NESTED_VIEW_NODE_EMPTY_NODE_CLASS, NESTED_NODE_VIEW_INNER_VIEW_DISPLAY_CONTAINER_CLASS, NESTED_NODE_VIEW_RENDER_DISPLAY_CONTAINER_CLASS } from '@ureeka-notebook/web-service';
+import { getPosType, isEditableInlineNodeWithContentNode, NestedViewNodeType, DATA_NODE_TYPE, NESTED_NODE_VIEW_INNER_VIEW_DISPLAY_CONTAINER_CLASS, NESTED_NODE_VIEW_RENDER_DISPLAY_CONTAINER_CLASS } from '@ureeka-notebook/web-service';
 
 import { AbstractNodeView } from 'notebookEditor/model/AbstractNodeView';
 
@@ -102,27 +102,5 @@ export abstract class AbstractNestedViewNodeView<NodeType extends NestedViewNode
   // showing the inner View. This can vary per implementation
   // and its meant to provide another representation of the Node's content when
   // the inner View is not active
-	public renderNodeContent() {
-		if(!this.renderDisplayContainer) return/*not set yet, nothing to do*/;
-
-		// get text string to render
-    const textString = getNestedViewNodeTextString(this.node);
-		if(textString.length < 1) {
-			this.dom.classList.add(NESTED_VIEW_NODE_EMPTY_NODE_CLASS);
-
-			// clear rendered DOM Nodes, since this Node is in an invalid state
-			while(this.renderDisplayContainer.firstChild){
-        this.renderDisplayContainer.firstChild.remove();
-      }
-      return/*nothing left to do*/;
-		} /* else -- not empty */
-
-    this.dom.classList.remove(NESTED_VIEW_NODE_EMPTY_NODE_CLASS);
-
-    // show the default representation of the content of this Node
-    this.renderDisplayContainer.firstChild?.remove();
-    const newRenderDisplayChild = document.createElement('span');
-          newRenderDisplayChild.innerHTML = `Length: ${textString.length}`;
-    this.renderDisplayContainer.appendChild(newRenderDisplayChild);
-  }
+	public abstract renderNodeContent(): void;
 }
